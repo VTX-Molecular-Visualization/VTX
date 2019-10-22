2651247b5eb3cb4def7f417235eff55fc0526dbf
Simon Guionniere <simon.guionniere@gmail.com>
2019-10-22 08:30:41 +0200

    CMake build with SDL2 [WIP]

--- cmake_VS2019_x64.bat
+++ cmake_VS2019_x64.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 ECHO [VTX] BUILDING SOLUTION FOR VS2019 x64
-cmake -B "_windows" -G "Visual Studio 16 2019" -DCMAKE_GENERATOR_PLATFORM=x64 --clean-first .
+cmake -B "_windows" -G "Visual Studio 16 2019" -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_TOOLCHAIN_FILE=D:\Drive\Freelance\Projets\CNAM\vcpkg\scripts\buildsystems\vcpkg.cmake --clean-first .
 ECHO [VTX] DONE
 PAUSE
+
+:: -DCMAKE_TOOLCHAIN_FILE="D:\\Drive\\Freelance\\Projets\\CNAM\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake"
