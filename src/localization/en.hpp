2a626610af4fa4a159de68de102c0a08d5964797
Simon Guionniere <simon.guionniere@gmail.com>
2019-10-31 04:05:13 +0100

    Language externalization [WIP]

---
+++ src/localization/en.hpp
@@ -0,0 +1,16 @@
+#ifndef __EN__
+#define __EN__
+
+#include <map>
+
+namespace VTX
+{
+	namespace Localization
+	{
+
+		std::map<std::string, std::string> en
+			= { { "MainMenu.Menu", "Menu" }, { "MainMenu.Menu", "Menu" } };
+
+	} // namespace Localization
+} // namespace VTX
+#endif
---
+++ src/localization/language.hpp
@@ -0,0 +1,19 @@
+#ifndef __LANGUAGE__
+#define __LANGUAGE__
+
+#include <iostream>
+#include <map>
+#include "en.hpp"
+
+namespace VTX
+{
+	namespace Localization
+	{
+		class Language
+		{
+		  public:
+			static std::string text( std::string ) {};
+		};
+	} 
+} // namespace VTX
+#endif
