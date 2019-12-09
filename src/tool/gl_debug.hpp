fb8f108c475eab501da75560e61872ba0c4c9d70
Simon Guionniere <simon.guionniere@gmail.com>
2019-12-09 04:30:58 +0100

    Log errors

--- src/tool/gl_debug.hpp
+++ src/tool/gl_debug.hpp
@@ -27,53 +27,40 @@ namespace VTX
 			switch ( p_source )
 			{
 			case GL_DEBUG_SOURCE_API: _source = "API"; break;
-
 			case GL_DEBUG_SOURCE_WINDOW_SYSTEM: _source = "WINDOW SYSTEM"; break;
-
 			case GL_DEBUG_SOURCE_SHADER_COMPILER: _source = "SHADER COMPILER"; break;
-
 			case GL_DEBUG_SOURCE_THIRD_PARTY: _source = "THIRD PARTY"; break;
-
 			case GL_DEBUG_SOURCE_APPLICATION: _source = "APPLICATION"; break;
-
 			case GL_DEBUG_SOURCE_OTHER: _source = "UNKNOWN"; break;
-
 			default: _source = "UNKNOWN"; break;
 			}
 
 			switch ( p_type )
 			{
 			case GL_DEBUG_TYPE_ERROR: _type = "ERROR"; break;
-
 			case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: _type = "DEPRECATED BEHAVIOR"; break;
-
 			case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: _type = "UDEFINED BEHAVIOR"; break;
-
 			case GL_DEBUG_TYPE_PORTABILITY: _type = "PORTABILITY"; break;
-
 			case GL_DEBUG_TYPE_PERFORMANCE: _type = "PERFORMANCE"; break;
-
 			case GL_DEBUG_TYPE_OTHER: _type = "OTHER"; break;
-
 			case GL_DEBUG_TYPE_MARKER: _type = "MARKER"; break;
-
 			default: _type = "UNKNOWN"; break;
 			}
 
 			switch ( p_severity )
 			{
-			case GL_DEBUG_SEVERITY_HIGH: _severity = "HIGH"; break;
+			case GL_DEBUG_SEVERITY_HIGH:
+				_severity = "HIGH";
+				{
+					std::printf(
+						"%d: %s of %s severity, raised from %s: %s\n", p_id, _type, _severity, _source, p_msg );
+				}
 
-			case GL_DEBUG_SEVERITY_MEDIUM: _severity = "MEDIUM"; break;
-
-			case GL_DEBUG_SEVERITY_LOW: _severity = "LOW"; break;
-
+			case GL_DEBUG_SEVERITY_MEDIUM: _severity = "MEDIUM";
+			case GL_DEBUG_SEVERITY_LOW: _severity = "LOW";
 			case GL_DEBUG_SEVERITY_NOTIFICATION: _severity = "NOTIFICATION"; break;
-
 			default: _severity = "UNKNOWN"; break;
 			}
-
-			std::printf( "%d: %s of %s severity, raised from %s: %s\n", p_id, _type, _severity, _source, p_msg );
 		}
 	} // namespace Tool
 
