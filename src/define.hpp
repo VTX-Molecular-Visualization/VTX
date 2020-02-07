b13e988e6f4b7ef59ef52c5a409751c4cedf5b4e
Maxime MARIA <maxime.maria@unilim.fr>
2020-02-07 20:51:17 +0100

    AO param for video

--- src/define.hpp
+++ src/define.hpp
@@ -81,13 +81,13 @@ namespace VTX
 	// TODO: choose relevent constants
 	constexpr float RENDERER_AO_RADIUS_MIN		  = 0.f;
 	constexpr float RENDERER_AO_RADIUS_MAX		  = 200.f;
-	constexpr float RENDERER_AO_RADIUS_DEFAULT	  = 80.f;
+	constexpr float RENDERER_AO_RADIUS_DEFAULT	  = 100.f;
 	constexpr int	RENDERER_AO_INTENSITY_MIN	  = 1;
 	constexpr int	RENDERER_AO_INTENSITY_MAX	  = 10;
 	constexpr int	RENDERER_AO_INTENSITY_DEFAULT = 3;
 	constexpr int	RENDERER_AO_BLUR_SIZE_MIN	  = 1;
 	constexpr int	RENDERER_AO_BLUR_SIZE_MAX	  = 99;
-	constexpr int	RENDERER_AO_BLUR_SIZE_DEFAULT = 9;
+	constexpr int	RENDERER_AO_BLUR_SIZE_DEFAULT = 3;
 
 	// Export video.
 	constexpr uint VIDEO_FPS = 60;
