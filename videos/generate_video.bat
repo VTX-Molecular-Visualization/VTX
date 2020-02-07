0ea5f28923c5e83e5c1a1dc0ae27c67c591d81f0
Simon Guionniere <simon.guionniere@gmail.com>
2020-02-07 22:00:52 +0100

    ffmpeg

--- videos/generate_video.bat
+++ videos/generate_video.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO [VTX] GENERATING VIDEO
-.\ffmpeg.exe -f image2 -framerate 60 -i snapshot%04d.png -vcodec libx264 video.mp4
+.\ffmpeg.exe -f image2 -framerate 60 -i snapshot%04d.png -vcodec -s 1920x1080 -crf 10 libx264 video.mp4
 ECHO [VTX] DONE
 PAUSE
