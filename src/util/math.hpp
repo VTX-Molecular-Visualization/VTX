2f7b224d58cd5cb02e7d87b13483f153e3a2256c
Simon Guionniere <simon.guionniere@gmail.com>
2020-03-20 20:16:16 +0100

    Cleanup math.hpp

--- src/util/math.hpp
+++ src/util/math.hpp
@@ -47,8 +47,7 @@ namespace VTX
 			static inline Quatd eulerToQuaternion( const double p_pitch, const double p_yaw, const double p_roll )
 			{
 				// https://www.wikiwand.com/en/Conversion_between_quaternions_and_Euler_angles
-
-				/*9
+				/*
 				float cy = cos( p_yaw * 0.5f );
 				float sy = sin( p_yaw * 0.5f );
 				float cp = cos( p_pitch * 0.5f );
@@ -64,50 +63,35 @@ namespace VTX
 
 				return q;
 				*/
-
-				// Exact same thing?
+				// Same thing did by glm.
 				Quatd q = eulerToQuaternion( Vec3d( p_pitch, p_yaw, p_roll ) );
 				return q;
 			}
 
 			static inline Vec3f quaternionToEuler( const Quatd & p_quaternion )
 			{
-				// https://stackoverflow.com/questions/53033620/how-to-convert-euler-angles-to-quaternions-and-get-the-same-euler-angles-back-fr
-				/*
-				const Quatf & q = p_quaternion;
-				float		  x = q.x;
-				float		  y = q.y;
-				float		  z = q.z;
-				float		  w = q.w;
-
-				float t0 = 2.f * ( w * x + y * z );
-				float t1 = 1.f - 2.f * ( x * x + y * y );
-				float t2 = 2.f * ( w * y - z * x );
-				t2		 = glm::clamp( t2, -1.f, 1.f );
-				float t3 = 2.f * ( w * z + x * y );
-				float t4 = 1.f - 2.f * ( y * y + z * z );
-
-				return Vec3f( atan2( t0, t1 ), asin( t2 ), atan2( t3, t4 ) );
-				*/
-
 				// https://www.wikiwand.com/en/Conversion_between_quaternions_and_Euler_angles
-
 				const Quatd & q = p_quaternion;
 				Vec3d		  angles;
 
-				// roll (x-axis rotation)
+				// Roll (x-axis rotation).
 				double sinr_cosp = 2 * ( q.w * q.x + q.y * q.z );
 				double cosr_cosp = 1 - 2 * ( q.x * q.x + q.y * q.y );
 				angles.z		 = std::atan2( sinr_cosp, cosr_cosp );
 
-				// pitch (y-axis rotation)
+				// Pitch (y-axis rotation)
 				double sinp = 2 * ( q.w * q.y - q.z * q.x );
 				if ( std::abs( sinp ) >= 1 )
-					angles.x = std::copysign( PI_2f / 2, sinp ); // use 90 degrees if out of range
+				{
+					// Use 90 degrees if out of range.
+					angles.x = std::copysign( PI_2f / 2, sinp );
+				}
 				else
+				{
 					angles.x = std::asin( sinp );
+				}
 
-				// yaw (z-axis rotation)
+				// Yaw (z-axis rotation).
 				double siny_cosp = 2 * ( q.w * q.z + q.x * q.y );
 				double cosy_cosp = 1 - 2 * ( q.y * q.y + q.z * q.z );
 				angles.y		 = std::atan2( siny_cosp, cosy_cosp );
@@ -120,20 +104,6 @@ namespace VTX
 			{
 				return glm::lerp( p_lhs, p_rhs, p_value );
 			}
-
-			/*
-			template<typename T>
-			static inline T slerp( const T & p_lhs, const T & p_rhs, const float p_value )
-			{
-				return glm::slerp( p_lhs, p_rhs, p_value );
-			}
-
-			template<typename T>
-			static inline T mix( const T & p_lhs, const T & p_rhs, const float p_value )
-			{
-				return glm::mix( p_lhs, p_rhs, p_value );
-			}
-			*/
 
 			template<typename T>
 			static T catmullRomInterpolation( const T &	  p_p0,
