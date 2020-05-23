45be4c6c3bba66a6ae4e781ff244853d214f87de
Maxime MARIA <maxime.maria@unilim.fr>
2020-05-23 09:36:28 +0200

    FXAA

--- src/renderer/gl/pass/shading.cpp
+++ src/renderer/gl/pass/shading.cpp
@@ -16,8 +16,10 @@ namespace VTX
 				glGenTextures( 1, &_shadingTexture );
 				glBindTexture( GL_TEXTURE_2D, _shadingTexture );
 				glTexStorage2D( GL_TEXTURE_2D, 1, GL_RGBA32F, p_width, p_height );
-				glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-				glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+				glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+				glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+				glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+				glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
 
 				glFramebufferTexture( GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, _shadingTexture, 0 );
 
