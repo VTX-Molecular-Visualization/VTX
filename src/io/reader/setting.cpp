da338864e5b1600b285a8742f7b4103befd1e987
Simon Guionniere <simon.guionniere@gmail.com>
2020-06-19 01:16:15 +0200

    Do not use implicit conversions during JSON reading (as adviced in json lib doc)

--- src/io/reader/setting.cpp
+++ src/io/reader/setting.cpp
@@ -16,33 +16,39 @@ namespace VTX
 				// TODO: check values before update, or force all values?
 
 				// Set new values with actions.
-				VTX_ACTION( new Action::Setting::ChangeTheme( json[ "THEME" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeDisplayMode( json[ "SYMBOL_DISPLAY_MODE" ] ) );
+				VTX_ACTION( new Action::Setting::ChangeTheme( json[ "THEME" ].get<Style::THEME>() ) );
+				VTX_ACTION( new Action::Setting::ChangeDisplayMode(
+					json[ "SYMBOL_DISPLAY_MODE" ].get<Style::SYMBOL_DISPLAY_MODE>() ) );
 
-				VTX_ACTION( new Action::Setting::ActiveRenderer( json[ "ACTIVE_RENDERER" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeRepresentation( json[ "REPRESENTATION" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeAtomsRadius( json[ "ATOMS_RADIUS" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeBondsRadius( json[ "BONDS_RADIUS" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeColorMode( json[ "COLOR_MODE" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeShading( json[ "SHADING" ] ) );
-				VTX_ACTION( new Action::Setting::ActiveVerticalSync( json[ "ACTIVE_VSYNC" ] ) );
-				VTX_ACTION( new Action::Setting::ActiveAO( json[ "ACTIVE_AO" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeAOIntensity( json[ "AO_INTENSITY" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeAOBlurSize( json[ "AO_BLUR_SIZE" ] ) );
-				VTX_ACTION( new Action::Setting::ActiveAA( json[ "ACTIVE_AA" ] ) );
-				VTX_ACTION( new Action::Setting::ActiveOutline( json[ "ACTIVE_OUTLINE" ] ) );
+				VTX_ACTION( new Action::Setting::ActiveRenderer( json[ "ACTIVE_RENDERER" ].get<bool>() ) );
+				VTX_ACTION( new Action::Setting::ChangeRepresentation(
+					json[ "REPRESENTATION" ].get<Generic::REPRESENTATION>() ) );
+				VTX_ACTION( new Action::Setting::ChangeAtomsRadius( json[ "ATOMS_RADIUS" ].get<float>() ) );
+				VTX_ACTION( new Action::Setting::ChangeBondsRadius( json[ "BONDS_RADIUS" ].get<float>() ) );
+				VTX_ACTION( new Action::Setting::ChangeColorMode( json[ "COLOR_MODE" ].get<Generic::COLOR_MODE>() ) );
+				VTX_ACTION( new Action::Setting::ChangeShading( json[ "SHADING" ].get<Renderer::SHADING>() ) );
+				VTX_ACTION( new Action::Setting::ActiveVerticalSync( json[ "ACTIVE_VSYNC" ].get<bool>() ) );
+				VTX_ACTION( new Action::Setting::ActiveAO( json[ "ACTIVE_AO" ].get<bool>() ) );
+				VTX_ACTION( new Action::Setting::ChangeAOIntensity( json[ "AO_INTENSITY" ].get<int>() ) );
+				VTX_ACTION( new Action::Setting::ChangeAOBlurSize( json[ "AO_BLUR_SIZE" ].get<int>() ) );
+				VTX_ACTION( new Action::Setting::ActiveAA( json[ "ACTIVE_AA" ].get<bool>() ) );
+				VTX_ACTION( new Action::Setting::ActiveOutline( json[ "ACTIVE_OUTLINE" ].get<bool>() ) );
 				//{ "OUTLINE_COLOR", VTX_SETTING().outlineColor },
 				//{ "BACKGROUND_COLOR", VTX_SETTING().backgroundColor },
 
-				VTX_ACTION( new Action::Setting::ChangeCameraClip( json[ "CAMERA_NEAR" ], json[ "CAMERA_FAR" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeCameraFov( json[ "CAMERA_FOV" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeCameraProjection( json[ "CAMERA_PERSPECTIVE" ] ) );
-
-				VTX_ACTION( new Action::Setting::ChangeTranslationSpeed( json[ "CONTROLLER_TRANSLATION_SPEED" ] ) );
 				VTX_ACTION(
-					new Action::Setting::ChangeTranslationFactorSpeed( json[ "CONTROLLER_TRANSLATION_FACTOR" ] ) );
-				VTX_ACTION( new Action::Setting::ChangeRotationSpeed( json[ "CONTROLLER_ROTATION_SPEED" ] ) );
-				VTX_ACTION( new Action::Setting::ActiveYAxisInversion( json[ "CONTROLLER_Y_AXIS_INVERTED" ] ) );
+					new Action::Setting::ChangeCameraClip( json[ "CAMERA_NEAR" ], json[ "CAMERA_FAR" ].get<float>() ) );
+				VTX_ACTION( new Action::Setting::ChangeCameraFov( json[ "CAMERA_FOV" ].get<float>() ) );
+				VTX_ACTION( new Action::Setting::ChangeCameraProjection( json[ "CAMERA_PERSPECTIVE" ].get<bool>() ) );
+
+				VTX_ACTION( new Action::Setting::ChangeTranslationSpeed(
+					json[ "CONTROLLER_TRANSLATION_SPEED" ].get<float>() ) );
+				VTX_ACTION( new Action::Setting::ChangeTranslationFactorSpeed(
+					json[ "CONTROLLER_TRANSLATION_FACTOR" ].get<float>() ) );
+				VTX_ACTION(
+					new Action::Setting::ChangeRotationSpeed( json[ "CONTROLLER_ROTATION_SPEED" ].get<float>() ) );
+				VTX_ACTION(
+					new Action::Setting::ActiveYAxisInversion( json[ "CONTROLLER_Y_AXIS_INVERTED" ].get<bool>() ) );
 
 				//{ "AUTO_ROTATE_SPEED", VTX_SETTING().autoRotationSpeed }
 			} // namespace Reader
