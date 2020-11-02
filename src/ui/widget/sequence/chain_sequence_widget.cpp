d7de6e4c0b11a6964b7124cb7f3a2dbaa7db920e
Nicodak <sacraioustille@orange.fr>
2020-11-02 15:32:41 +0100

    - Sequence scale start from original chain index

--- src/ui/widget/sequence/chain_sequence_widget.cpp
+++ src/ui/widget/sequence/chain_sequence_widget.cpp
@@ -62,14 +62,28 @@ namespace VTX
 					const int residueCount = _model->getResidueCount();
 					QString	  scaleTxt	   = QString( residueCount, ' ' );
 
-					if ( residueCount >= 1 )
-						scaleTxt[ 0 ] = '1';
+					if ( residueCount <= 0 )
+						return;
 
-					for ( int i = Style::SEQUENCE_CHAIN_SCALE_STEP; i < residueCount; i += Style::SEQUENCE_CHAIN_SCALE_STEP )
+					const int		  startIndex		= _model->getMoleculePtr()->getResidue( _model->getIndexFirstResidue() ).getIndexInOriginalChain();
+					const std::string firstIndexStr		= std::to_string( startIndex );
+					const int		  firstIndexStrSize = (int)firstIndexStr.size();
+
+					// We display the index of the first label (in original chain)
+					for ( int i = 0; i < firstIndexStrSize; i++ )
+						scaleTxt[ i ] = firstIndexStr[ i ];
+
+					// Then, the second one will be the next multiple of Style::SEQUENCE_CHAIN_SCALE_STEP which does not overlay or stick with the chars of the first index
+					int secondIndex = Style::SEQUENCE_CHAIN_SCALE_STEP - ( startIndex % Style::SEQUENCE_CHAIN_SCALE_STEP );
+					if ( secondIndex <= ( firstIndexStrSize + ( firstIndexStrSize / 2 ) ) )
+						secondIndex += Style::SEQUENCE_CHAIN_SCALE_STEP;
+
+					// After that, we draw a label every Style::SEQUENCE_CHAIN_SCALE_STEP
+					for ( int i = secondIndex; i < residueCount; i += Style::SEQUENCE_CHAIN_SCALE_STEP )
 					{
-						const std::string indexTxt	   = std::to_string( i );
+						const std::string indexTxt	   = std::to_string( startIndex + i );
 						const int		  indexTxtSize = (int)indexTxt.size();
-						const int		  indexOffset  = ( ( indexTxtSize - 1 ) / 2 ) + 1;
+						const int		  indexOffset  = ( indexTxtSize - 1 ) / 2;
 
 						for ( int j = 0; j < indexTxtSize; j++ )
 							scaleTxt[ i + j - indexOffset ] = indexTxt[ j ];
