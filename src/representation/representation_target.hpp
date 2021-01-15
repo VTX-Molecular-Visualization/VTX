de016d9abc9b2cf86072e585b8610418fe7d6783
Nicodak <sacraioustille@orange.fr>
2021-01-15 01:39:06 +0100

    - Debug buffer filling in representation_target

--- src/representation/representation_target.hpp
+++ src/representation/representation_target.hpp
@@ -24,7 +24,7 @@ namespace VTX
 					if ( p_count <= 0 )
 						return;
 
-					if ( _lastIndex + 1 == p_firstIndex )
+					if ( _lastIndex == p_firstIndex )
 					{
 						_range[ _lastKey ] += p_count;
 						_lastIndex += p_count;
@@ -37,6 +37,40 @@ namespace VTX
 						_lastIndex = p_firstIndex + p_count;
 					}
 				}
+				void appendWithBrowse( const std::pair<uint, uint> & p_data )
+				{
+					appendWithBrowse( p_data.first, p_data.second );
+				};
+				void appendWithBrowse( const uint p_firstIndex, const uint p_count )
+				{
+					if ( p_count <= 0 )
+						return;
+
+					for ( std::map<uint, uint>::iterator & it = _range.begin(); it != _range.end(); it++ )
+					{
+						// Already in buffer
+						if ( it->first <= p_firstIndex && p_firstIndex < it->first + it->second )
+							return;
+
+						if ( ( it->first + it->second ) == p_firstIndex )
+						{
+							it->second += p_count;
+
+							std::map<uint, uint>::iterator nextIt = it;
+							nextIt++;
+							if ( nextIt != _range.end() && it->second == nextIt->first )
+							{
+								it->second += nextIt->second;
+								_range.erase( nextIt );
+							}
+
+							return;
+						}
+					}
+
+					_range.emplace( p_firstIndex, p_count );
+				}
+
 				const std::map<uint, uint> & getRange() const { return _range; }
 
 			  private:
@@ -48,16 +82,18 @@ namespace VTX
 		  public:
 			RepresentationTarget() {};
 
-			void appendAtoms( const std::pair<uint, uint> & p_atomData ) { _atoms.append( p_atomData ); };
-			void appendBonds( const std::pair<uint, uint> & p_bondsRange, const std::vector<uint> & p_indexExtraBondStart, const std::vector<uint> & p_indexExtraBondEnd )
+			void appendAtoms( const std::pair<uint, uint> & p_atomData ) { _atoms.appendWithBrowse( p_atomData ); };
+			void appendBonds( const std::pair<uint, uint> & p_bondsRange,
+							  const std::vector<uint> &		p_indexExtraBondStart,
+							  const std::vector<uint> &		p_indexExtraBondEnd )
 			{
-				_bonds.append( p_bondsRange );
+				_bonds.appendWithBrowse( p_bondsRange );
 
 				for ( uint indexBond : p_indexExtraBondStart )
-					_bonds.append( indexBond * 2, 2 );
+					_bonds.appendWithBrowse( indexBond * 2, 2 );
 
 				for ( uint indexBond : p_indexExtraBondEnd )
-					_bonds.append( indexBond * 2, 2 );
+					_bonds.appendWithBrowse( indexBond * 2, 2 );
 			};
 			void appendRibbons( const std::pair<uint, uint> & p_ribbonRange ) { _ribbons.append( p_ribbonRange ); }
 
