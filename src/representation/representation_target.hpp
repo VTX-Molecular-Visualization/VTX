4867f7806c8006e995b15ef6bf3ee85b1d096a31
Nicodak <sacraioustille@orange.fr>
2021-01-15 12:25:30 +0100

    - clean representation_target

--- src/representation/representation_target.hpp
+++ src/representation/representation_target.hpp
@@ -24,43 +24,26 @@ namespace VTX
 					if ( p_count <= 0 )
 						return;
 
-					if ( _lastIndex == p_firstIndex )
-					{
-						_range[ _lastKey ] += p_count;
-						_lastIndex += p_count;
-					}
-					else
-					{
-						_range.emplace( p_firstIndex, p_count );
-
-						_lastKey   = p_firstIndex;
-						_lastIndex = p_firstIndex + p_count;
-					}
-				}
-				void appendWithBrowse( const std::pair<uint, uint> & p_data )
-				{
-					appendWithBrowse( p_data.first, p_data.second );
-				};
-				void appendWithBrowse( const uint p_firstIndex, const uint p_count )
-				{
-					if ( p_count <= 0 )
-						return;
-
 					for ( std::map<uint, uint>::iterator & it = _range.begin(); it != _range.end(); it++ )
 					{
+						const uint itFirstIndex = it->first;
+						uint &	   itCount		= it->second;
+						const uint itNextIndex	= itFirstIndex + itCount;
+
 						// Already in buffer
-						if ( it->first <= p_firstIndex && p_firstIndex < it->first + it->second )
+						if ( itFirstIndex <= p_firstIndex && p_firstIndex < itNextIndex )
 							return;
 
-						if ( ( it->first + it->second ) == p_firstIndex )
+						if ( itNextIndex == p_firstIndex )
 						{
-							it->second += p_count;
+							itCount += p_count;
 
+							// If new last index join the next range, fusion it
 							std::map<uint, uint>::iterator nextIt = it;
 							nextIt++;
 							if ( nextIt != _range.end() && it->second == nextIt->first )
 							{
-								it->second += nextIt->second;
+								itCount += nextIt->second;
 								_range.erase( nextIt );
 							}
 
@@ -74,26 +57,24 @@ namespace VTX
 				const std::map<uint, uint> & getRange() const { return _range; }
 
 			  private:
-				std::map<uint, uint> _range		= std::map<uint, uint>();
-				uint				 _lastIndex = -2;
-				uint				 _lastKey	= -2;
+				std::map<uint, uint> _range = std::map<uint, uint>();
 			};
 
 		  public:
 			RepresentationTarget() {};
 
-			void appendAtoms( const std::pair<uint, uint> & p_atomData ) { _atoms.appendWithBrowse( p_atomData ); };
+			void appendAtoms( const std::pair<uint, uint> & p_atomData ) { _atoms.append( p_atomData ); };
 			void appendBonds( const std::pair<uint, uint> & p_bondsRange,
 							  const std::vector<uint> &		p_indexExtraBondStart,
 							  const std::vector<uint> &		p_indexExtraBondEnd )
 			{
-				_bonds.appendWithBrowse( p_bondsRange );
+				_bonds.append( p_bondsRange );
 
 				for ( uint indexBond : p_indexExtraBondStart )
-					_bonds.appendWithBrowse( indexBond * 2, 2 );
+					_bonds.append( indexBond * 2, 2 );
 
 				for ( uint indexBond : p_indexExtraBondEnd )
-					_bonds.appendWithBrowse( indexBond * 2, 2 );
+					_bonds.append( indexBond * 2, 2 );
 			};
 			void appendRibbons( const std::pair<uint, uint> & p_ribbonRange ) { _ribbons.append( p_ribbonRange ); }
 
