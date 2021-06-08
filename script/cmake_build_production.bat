5c49a68f9931acc729be285176ff0a50fd609e6d
Simon Guionniere <simon.guionniere@gmail.com>
2021-06-08 19:55:48 +0200

    Build script production for real this time

--- script/cmake_build_production.bat
+++ script/cmake_build_production.bat
@@ -1,4 +1,5 @@
-cmake --build ../_windows --config Release -- /p:CustomPreprocessorDefinitions=VTX_PRODUCTION
+cmake .. -B "../_windows" -G "Visual Studio 16 2019" -DCMAKE_GENERATOR_PLATFORM=x64 -DDEFINE_PRODUCTION=ON .
+cmake --build ../_windows --config Release --clean-first
 robocopy ../_windows/build build /nfl /ndl /njh /E
 rmdir /s /q build\bin\Debug
 rmdir /s /q build\bin\RelWithDebInfo
@@ -8,4 +9,5 @@ rmdir /s /q build\logs
 del /s /q build\config.ini
 del /s /q build\setting.json
 del /s /q build\libraries\render_effects\Default
+cmake .. -B "../_windows" -G "Visual Studio 16 2019" -DCMAKE_GENERATOR_PLATFORM=x64 -DDEFINE_PRODUCTION=OFF .
 PAUSE
