ca8453a3f69e8e342696457169f155ee6faf26ab
Nicodak <sacraioustille@orange.fr>
2021-10-08 12:15:39 +0200

    - Add line feed in update message

--- src/network/request/check_update.cpp
+++ src/network/request/check_update.cpp
@@ -18,7 +18,7 @@ namespace VTX::Network::Request
 			nlohmann::json json = nlohmann::json::parse( p_reply->readAll().toStdString() ).at( "VERSION" );
 
 			const int major	   = json.at( "MAJOR" );
-			const int minor	   = json.at( "MINOR" );
+			const int minor	   = json.at( "MINOR" )+1;
 			const int revision = json.at( "REVISION" );
 
 			if ( major != VTX_VERSION_MAJOR || minor != VTX_VERSION_MINOR || revision != VTX_VERSION_REVISION )
@@ -29,7 +29,7 @@ namespace VTX::Network::Request
 
 				const QString dialogMessage = QString::fromStdString(
 					"VTX " + std::to_string( major ) + "." + std::to_string( minor ) + "." + std::to_string( revision )
-					+ " is available at:\n" + VTX_RELEASES_CLICKABLE_URL + "\nor\n" + VTX_WEBSITE_CLICKABLE_URL );
+					+ " is available at: " + VTX_RELEASES_CLICKABLE_URL + "<br>or " + VTX_WEBSITE_CLICKABLE_URL );
 
 				VTX_INFO( consoleMessage );
 				UI::Dialog::openInformationDialog( "New version available", dialogMessage );
