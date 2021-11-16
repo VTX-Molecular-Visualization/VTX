030795cea17ecf053ccf5eda0338f9d383c2dd70
Nicodak <sacraioustille@orange.fr>
2021-11-16 13:39:13 +0100

    - Use ratio instead of real snapshot for preview

--- src/ui/widget/custom_widget/render_preview_widget.cpp
+++ src/ui/widget/custom_widget/render_preview_widget.cpp
@@ -1,38 +1,33 @@
 #include "render_preview_widget.hpp"
+#include "style.hpp"
 #include "worker/snapshoter.hpp"
 #include "worker/worker_manager.hpp"
-#include "style.hpp"
 
 namespace VTX::UI::Widget::CustomWidget
 {
-	RenderPreviewWidget::RenderPreviewWidget( QWidget * p_parent ) :
-		BaseManualWidget<QLabel>( p_parent )
-	{
-	}
+	RenderPreviewWidget::RenderPreviewWidget( QWidget * p_parent ) : BaseManualWidget<QLabel>( p_parent ) {}
 
-	void RenderPreviewWidget::_setupUi( const QString & p_name )
-	{
-		BaseManualWidget::_setupUi( p_name );
-	}
-	void RenderPreviewWidget::_setupSlots()
-	{
-	}
-	
-	QSize RenderPreviewWidget::sizeHint() const 
-	{ 
-		return _previewPixmpap.size();
-	}
+	void RenderPreviewWidget::_setupUi( const QString & p_name ) { BaseManualWidget::_setupUi( p_name ); }
+	void RenderPreviewWidget::_setupSlots() {}
 
+	QSize RenderPreviewWidget::sizeHint() const { return _previewPixmpap.size(); }
 
-	void RenderPreviewWidget::takeSnapshot( const IO::Struct::ImageExport & p_exportData ) 
+	void RenderPreviewWidget::takeSnapshot( const IO::Struct::ImageExport & p_exportData )
 	{
-		QImage					preview = QImage();
+		QImage preview = QImage();
+
+		IO::Struct::ImageExport	  snapshotData = p_exportData;
+		const std::pair<int, int> exportSize   = snapshotData.getSize();
+		const float				  exportRatio  = exportSize.first / float( exportSize.second );
+
+		snapshotData.setSize( Style::EXPORT_IMAGE_PREVIEW_SIZE.width() * exportRatio,
+							  Style::EXPORT_IMAGE_PREVIEW_SIZE.height() );
 
 		Worker::Snapshoter * const snapshoter
-			= new Worker::Snapshoter( Worker::Snapshoter::MODE::GL, &preview, p_exportData );
+			= new Worker::Snapshoter( Worker::Snapshoter::MODE::GL, &preview, snapshotData );
 		VTX_WORKER( snapshoter );
 
-		const QSize imgSize = Style::EXPORT_IMAGE_PREVIEW_SIZE;
+		const QSize	 imgSize = Style::EXPORT_IMAGE_PREVIEW_SIZE;
 		const QImage scaledPreviewImg
 			= preview.scaled( imgSize.width(), imgSize.height(), Qt::AspectRatioMode::KeepAspectRatio );
 
