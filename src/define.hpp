11d4054bd7e74611edffeec1fc476671d84cf817
Simon Guionnière <simon.guionniere@gmail.com>
2021-12-08 18:26:43 +0100

    Update version + check update

--- src/define.hpp
+++ src/define.hpp
@@ -34,8 +34,8 @@ namespace VTX
 	// VTX.
 	const std::string VTX_PROJECT_NAME	   = "VTX";
 	constexpr int	  VTX_VERSION_MAJOR	   = 0;
-	constexpr int	  VTX_VERSION_MINOR	   = 1;
-	constexpr int	  VTX_VERSION_REVISION = 2;
+	constexpr int	  VTX_VERSION_MINOR	   = 2;
+	constexpr int	  VTX_VERSION_REVISION = 0;
 
 	const std::string VTX_WEBSITE_URL			= "https://vtx.drugdesign.fr";
 	const std::string VTX_VERSION_URL			= VTX_WEBSITE_URL + "/vtx.json";
