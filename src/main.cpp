3957c72b3eb7c6b7a92f0c954d6aa2fce356a29b
Simon Guionnière <simon.guionniere@gmail.com>
2023-03-27 11:22:53 +0200

    Revert

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,6 @@ int main( int p_argc, char * p_argv[] )
 {
 	try
 	{
-		std::cout << "Starting" << std::endl;
 		// Setup some Qt static configuration.
 		QCoreApplication::setAttribute( Qt::AA_UseDesktopOpenGL );
 		QCoreApplication::setAttribute( Qt::AA_DontCheckOpenGLContextThreadAffinity );
