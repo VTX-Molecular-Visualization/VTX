67e5748b51c6a80d7d30a15ac13e4e37a319ecfb
Nicolas Dacquay <nicolas.dacquay@lecnam.net>
2023-04-03 17:15:26 +0200

    - Update contextual menus

--- src/ui/widget/contextual_menu/contextual_menu_scene.cpp
+++ src/ui/widget/contextual_menu/contextual_menu_scene.cpp
@@ -1,8 +1,11 @@
 #include "contextual_menu_scene.hpp"
 #include "action/action_manager.hpp"
+#include "action/main.hpp"
 #include "action/scene.hpp"
 #include "action/viewpoint.hpp"
+#include "io/struct/scene_path_data.hpp"
 #include "ui/dialog.hpp"
+#include "vtx_app.hpp"
 
 namespace VTX::UI::Widget::ContextualMenu
 {
@@ -15,8 +18,10 @@ namespace VTX::UI::Widget::ContextualMenu
 		addSection( "Loading" );
 		addAction( "Load Molecule", this, &ContextualMenuScene::_loadMoleculeAction );
 		addAction( "Download Molecule", this, &ContextualMenuScene::_downloadMoleculeAction );
+		addAction( "Save Session", this, &ContextualMenuScene::_saveSessionAction );
+		addAction( "Save Session As...", this, &ContextualMenuScene::_saveAsSessionAction );
 
-		addSection( "Actions" );
+		addSection( "Action" );
 		addAction( "Show All", this, &ContextualMenuScene::_showAllMoleculesAction );
 		addSection( "Viewpoints" );
 		addAction( "Add Viewpoint", this, &ContextualMenuScene::_addViewpointAction );
@@ -28,6 +33,11 @@ namespace VTX::UI::Widget::ContextualMenu
 
 	void ContextualMenuScene::_loadMoleculeAction() { UI::Dialog::openLoadMoleculeDialog(); }
 	void ContextualMenuScene::_downloadMoleculeAction() { UI::Dialog::openDownloadMoleculeDialog(); }
+	void ContextualMenuScene::_saveSessionAction()
+	{
+		VTX_ACTION( new Action::Main::Save( VTXApp::get().getScenePathData().getCurrentPath() ) );
+	}
+	void ContextualMenuScene::_saveAsSessionAction() { Dialog::openSaveSessionDialog(); }
 	void ContextualMenuScene::_showAllMoleculesAction() const { VTX_ACTION( new Action::Scene::ShowAllMolecules() ); }
 	void ContextualMenuScene::_addViewpointAction() { VTX_ACTION( new Action::Viewpoint::Create() ); }
 	void ContextualMenuScene::_clearAction() { VTX_ACTION( new Action::Scene::ResetScene() ); }
