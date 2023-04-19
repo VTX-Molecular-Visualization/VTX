8b481f3f512dffef67cc48fbdf0f7f496ab63aee
Simon Guionnière <simon.guionniere@gmail.com>
2023-04-19 16:15:28 +0200

    Logger namespace

--- lib/util/include/util/logger.hpp
+++ lib/util/include/util/logger.hpp
@@ -6,53 +6,56 @@
 #include <memory>
 #include <spdlog/spdlog.h>
 
-namespace VTX::Util
+namespace VTX
 {
-	class Logger final : public Generic::BaseStaticSingleton<Logger>
+	namespace Util
 	{
-	  public:
-		Logger( StructPrivacyToken p_token );
-		Logger( std::initializer_list<int> ) = delete;
+		class Logger final : public Generic::BaseStaticSingleton<Logger>
+		{
+		  public:
+			Logger( StructPrivacyToken p_token );
+			Logger( std::initializer_list<int> ) = delete;
 
-		void init( const std::filesystem::path & );
-	};
+			void init( const std::filesystem::path & );
+		};
 
-} // namespace VTX::Util
+	} // namespace Util
 
-template<typename... Args>
-inline void VTX_TRACE( const std::string & p_fmt, const Args &... p_args )
-{
-	spdlog::log( spdlog::level::trace, p_fmt, p_args... );
-}
+	template<typename... Args>
+	inline void VTX_TRACE( const std::string & p_fmt, const Args &... p_args )
+	{
+		spdlog::log( spdlog::level::trace, p_fmt, p_args... );
+	}
 
-template<typename... Args>
-inline void VTX_DEBUG( const std::string & p_fmt, const Args &... p_args )
-{
-	spdlog::log( spdlog::level::debug, p_fmt, p_args... );
-}
+	template<typename... Args>
+	inline void VTX_DEBUG( const std::string & p_fmt, const Args &... p_args )
+	{
+		spdlog::log( spdlog::level::debug, p_fmt, p_args... );
+	}
 
-template<typename... Args>
-inline void VTX_INFO( const std::string & p_fmt, const Args &... p_args )
-{
-	spdlog::log( spdlog::level::info, p_fmt, p_args... );
-}
+	template<typename... Args>
+	inline void VTX_INFO( const std::string & p_fmt, const Args &... p_args )
+	{
+		spdlog::log( spdlog::level::info, p_fmt, p_args... );
+	}
 
-template<typename... Args>
-inline void VTX_WARNING( const std::string & p_fmt, const Args &... p_args )
-{
-	spdlog::log( spdlog::level::warn, p_fmt, p_args... );
-}
+	template<typename... Args>
+	inline void VTX_WARNING( const std::string & p_fmt, const Args &... p_args )
+	{
+		spdlog::log( spdlog::level::warn, p_fmt, p_args... );
+	}
 
-template<typename... Args>
-inline void VTX_ERROR( const std::string & p_fmt, const Args &... p_args )
-{
-	spdlog::log( spdlog::level::err, p_fmt, p_args... );
-}
+	template<typename... Args>
+	inline void VTX_ERROR( const std::string & p_fmt, const Args &... p_args )
+	{
+		spdlog::log( spdlog::level::err, p_fmt, p_args... );
+	}
 
-template<typename... Args>
-inline void VTX_CRITICAL( const std::string & p_fmt, const Args &... p_args )
-{
-	spdlog::log( spdlog::level::critical, p_fmt, p_args... );
-}
+	template<typename... Args>
+	inline void VTX_CRITICAL( const std::string & p_fmt, const Args &... p_args )
+	{
+		spdlog::log( spdlog::level::critical, p_fmt, p_args... );
+	}
+} // namespace VTX
 
 #endif
