30b3032bf9e4c39328546e650f63725a1b82a56c
Simon Guionnière <simon.guionniere@gmail.com>
2023-04-27 21:30:04 +0200

    Backup buffers

---
+++ lib/renderer/old/buffer/secondary_structure.hpp
@@ -0,0 +1,56 @@
+#ifndef __VTX_BUFFER_SECONDARY_STRUCTURE__
+#define __VTX_BUFFER_SECONDARY_STRUCTURE__
+
+#include "base_buffer_opengl.hpp"
+#include "app/old_app/color/rgba.hpp"
+#include <util/types.hpp>
+#include "app/core/model/base_model.hpp"
+#include "app/old_app/renderer/gl/vertex_array.hpp"
+#include <vector>
+
+namespace VTX
+{
+	namespace Buffer
+	{
+		class SecondaryStructure : public BaseBufferOpenGL
+		{
+		  public:
+			SecondaryStructure()  = default;
+			~SecondaryStructure() = default;
+
+			void setControlPointPositions( const std::vector<Vec4f> & );
+			void setControlPointDirections( const std::vector<Vec3f> & );
+			void setSecondaryStructures( const std::vector<uint> & );
+			void setColors( const std::vector<Color::Rgba> & );
+			void setVisibilities( const std::vector<uint> & );
+			void setSelections( const std::vector<uint> & );
+			void setIds( const std::vector<App::Core::Model::ID> & );
+			void setIndices( const std::vector<uint> & );
+
+		  protected:
+			void _generate() override;
+
+		  private:
+			enum ATTRIBUTE_LOCATION
+			{
+				CONTROL_POINT_POSITION		= 0,
+				CONTROL_POINT_DIRECTION		= 1,
+				RESIDUE_SECONDARY_STRUCTURE = 2,
+				RESIDUE_COLOR				= 3,
+				RESIDUE_VISIBILITY			= 4,
+				RESIDUE_SELECTION			= 5,
+				RESIDUE_ID					= 6,
+			};
+
+			Renderer::GL::Buffer _vboPositions			 = Renderer::GL::Buffer();
+			Renderer::GL::Buffer _vboDirections			 = Renderer::GL::Buffer();
+			Renderer::GL::Buffer _vboSecondaryStructures = Renderer::GL::Buffer();
+			Renderer::GL::Buffer _vboColors				 = Renderer::GL::Buffer();
+			Renderer::GL::Buffer _vboVisibilities		 = Renderer::GL::Buffer();
+			Renderer::GL::Buffer _vboSelections			 = Renderer::GL::Buffer();
+			Renderer::GL::Buffer _vboIds				 = Renderer::GL::Buffer();
+			Renderer::GL::Buffer _ibo					 = Renderer::GL::Buffer();
+		};
+	} // namespace Buffer
+} // namespace VTX
+#endif
