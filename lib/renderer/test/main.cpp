5deff28791b497d9fa456d1d1ace64bbfa8ece08
Simon Guionnière <simon.guionniere@gmail.com>
2023-05-18 22:44:00 +0200

    Trying without visible window

--- lib/renderer/test/main.cpp
+++ lib/renderer/test/main.cpp
@@ -12,6 +12,7 @@ TEST_CASE( "Renderer::GL", "[renderer]" )
 	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
 	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 5 );
 	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+	glfwWindowHint( GLFW_VISIBLE, GLFW_FALSE );
 
 	GLFWwindow * const window = glfwCreateWindow( 800, 600, "VTX_RENDERER_TEST", nullptr, nullptr );
 
