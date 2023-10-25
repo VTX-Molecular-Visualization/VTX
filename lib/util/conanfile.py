ada2636f68bbd914f50cf992d9942c15363c3b14
Simon Guionnière <simon.guionniere@gmail.com>
2023-10-25 11:49:21 +0200

    Add cmake in dependencies

--- lib/util/conanfile.py
+++ lib/util/conanfile.py
@@ -1,9 +1,17 @@
 from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps
 
-class CompressorRecipe(ConanFile):
+class VTXUtilRecipe(ConanFile):
+    name = "VTXUtil"
+    version = "1.0"
+    package_type = "library"
+    
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
-
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+    
     def requirements(self):
         self.requires("glm/0.9.9.8")
         self.requires("spdlog/1.12.0")
@@ -11,6 +19,33 @@ class CompressorRecipe(ConanFile):
         self.requires("magic_enum/0.9.3")
         self.requires("asio/1.28.1")
         self.requires("catch2/3.4.0")
+        
+    def build_requirements(self):
+        self.tool_requires("cmake/3.27.7")
+    
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
 
-    #def build_requirements(self):
-    #    self.tool_requires("cmake/3.27.7")
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["VTXUtil"]
+
