d0a62f4efc8154a436e0c3c3d8ba93d89e4b0141
Simon Guionnière <simon.guionniere@gmail.com>
2023-10-26 12:42:14 +0200

    Conan module Core

--- lib/renderer/bench/conanfile.py
+++ lib/renderer/bench/conanfile.py
@@ -1,13 +1,18 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMake
 
-class VTXUtilTestConan(ConanFile):
+class VTXRendererBenchRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     package_type = "application"
     
     generators = "CMakeToolchain", "CMakeDeps"
     
-    requires = "vtx_util/1.0", "catch2/3.4.0"
+    def requirements(self):
+        self.requires("vtx_util/1.0")
+        self.requires("vtx_renderer/1.0")
+        self.requires("vtx_core/1.0")
+        self.requires("vtx_io/1.0")
+        self.requires("sdl/2.28.3")         
         
     def layout(self):
         cmake_layout(self)
