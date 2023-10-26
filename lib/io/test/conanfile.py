85fd51d211d0b2d9447284f312f77fde7155ee85
Simon Guionnière <simon.guionniere@gmail.com>
2023-10-26 13:18:47 +0200

    Conan IO module

---
+++ lib/io/test/conanfile.py
@@ -0,0 +1,22 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMake
+
+class VTXIOTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    package_type = "application"
+    
+    generators = "CMakeToolchain", "CMakeDeps"
+    
+    def requirements(self):
+        self.requires("vtx_util/1.0")
+        self.requires("vtx_core/1.0")
+        self.requires("vtx_io/1.0")
+        self.requires("catch2/3.4.0")
+        
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
