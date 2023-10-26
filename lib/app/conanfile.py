bf3df26f3c415b63528528d49611b1100f84f157
Simon Guionnière <simon.guionniere@gmail.com>
2023-10-26 17:38:32 +0200

    Conan module App wip

---
+++ lib/app/conanfile.py
@@ -0,0 +1,45 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+
+class VTXAppRecipe(ConanFile):
+    name = "vtx_app"
+    version = "1.0"
+    package_type = "library"
+    
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    
+    generators = "CMakeDeps", "CMakeToolchain"
+    
+    #exports_sources = "CMakeLists.txt", "src/*", "include/*"
+        
+    def requirements(self):
+        self.requires("vtx_util/1.0")
+        self.requires("vtx_renderer/1.0")
+        self.requires("vtx_io/1.0")
+        self.requires("vtx_core/1.0")
+        self.requires("entt/3.12.2")
+     
+    #TODO: get CMake?
+    #def build_requirements(self):
+        
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["vtx_app"]
+
---
+++ lib/app/test/conanfile.py
@@ -0,0 +1,20 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMake
+
+class VTXUtilTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    package_type = "application"
+    
+    generators = "CMakeToolchain", "CMakeDeps"
+    
+    def requirements(self):
+        self.requires("vtx_util/1.0")
+        self.requires("catch2/3.4.0")
+        
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
