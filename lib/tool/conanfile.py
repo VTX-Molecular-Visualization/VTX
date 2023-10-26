770e40b97ac5b911c14fa7e05a618341a6c5b0b0
Simon Guionnière <simon.guionniere@gmail.com>
2023-10-26 18:10:22 +0200

    Conan module UI wip

---
+++ lib/tool/conanfile.py
@@ -0,0 +1,40 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+
+class VTXToolRecipe(ConanFile):
+    name = "vtx_tool"
+    version = "1.0"
+    package_type = "library"
+    
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    
+    generators = "CMakeDeps", "CMakeToolchain"
+    
+    #exports_sources = "CMakeLists.txt", "src/*", "include/*"
+        
+    def requirements(self):
+        self.requires("vtx_util/1.0")
+        self.requires("vtx_app/1.0")
+        self.requires("vtx_ui/1.0")
+        
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["vtx_tool"]
+
