ab901f5f629bdcd6aab849ce9100ac2fd240c7c3
Simon Guionnière <simon.guionniere@gmail.com>
2023-11-09 00:56:59 +0100

    CI: missing sources

--- lib/renderer/bench/conanfile.py
+++ lib/renderer/bench/conanfile.py
@@ -12,6 +12,8 @@ class VTXRendererBenchRecipe(ConanFile):
     
     generators = "CMakeDeps"
     
+    exports_sources = "CMakeLists.txt", "src/*", "cmake/*"
+    
     def requirements(self):
         self.requires("vtx_util/1.0")
         self.requires("vtx_renderer/1.0")
