8579e91caa43662ff5ea9c97ac391960be42dce8
Valentin Guillaume <valent.guillaume@gmail.com>
2023-11-17 15:35:32 +0100

    Fixing Qt6 dll management on build

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,21 @@ from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 from conan.tools.files import copy
 
+import shutil
+from pathlib import Path
+
+def move_dlls__ui(dest: Path) :
+    """
+    Assumes that dependencies dlls have been copied into default location beforehand
+        Default location should be the same directory tree that the lib itself, relative to this file's folder
+    """
+    for file in (dest / "lib" / "ui" / "build").iterdir():
+        if (file.is_dir()) :
+            shutil.copytree(file, dest / file.name)
+        elif (file.is_file()) :
+            shutil.copy2(file,  dest / file.name)
+    shutil.rmtree(dest / "lib" / "ui") # cleanup
+    
 
 class VTXRecipe(ConanFile):
     name = "vtx"
@@ -35,22 +50,23 @@ class VTXRecipe(ConanFile):
         tc.cache_variables["PATH_PYTHON_MODULE"] = path_python_module
         tc.generate()
 
-        copy(self, "*.dll", self.dependencies["vtx_ui"].cpp_info.bindir, self.build_folder)
-
-
     def layout(self):
         cmake_layout(self)
 
     def build(self):
-        
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-
+        
+        # Copy dll from dependencies into exe dir into [exe_dir]/lib/[module]/build/[build_type]
         build_type = self.settings.get_safe("build_type", default="Release")
         exe_dir = os.path.join(self.build_folder, build_type)
-        copy(self.package_folder, "*.dll", self.package_folder, exe_dir, keep_path=False)
-        
+        copy(self, "*.dll", self.package_folder, exe_dir)
+
+        # Put dependencies dlls into exe dir
+        move_dlls__ui(Path(exe_dir))
+
+        shutil.rmtree(Path(exe_dir) / "lib" ) # cleanup
 
     def package(self):
         cmake = CMake(self)
