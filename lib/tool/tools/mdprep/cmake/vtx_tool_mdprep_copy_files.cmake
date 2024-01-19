bb5f95b5eca66b9e5e048a459fea1b31b93a7645
Valentin Guillaume <valent.guillaume@gmail.com>
2024-01-19 13:45:37 +0100

    WIP - decoupling file copy away from package consumers

--- lib/tool/tools/mdprep/cmake/vtx_tool_mdprep_copy_files.cmake
+++ lib/tool/tools/mdprep/cmake/vtx_tool_mdprep_copy_files.cmake
@@ -1,5 +1,1 @@
-
-get_filename_component(_vtx_tool_mdprep_top_dir "./external/gromacs/share/top" ABSOLUTE )
-message ("### From vtx_tool_mdprep_copy_files.cmake _vtx_tool_mdprep_top_dir <${_vtx_tool_mdprep_top_dir}>")
-message ("Now calling register_build_directory_copy")
-register_build_directory_copy("${_vtx_tool_mdprep_top_dir}")
+register_build_directory_copy("${CMAKE_CURRENT_LIST_DIR}/../external/gromacs/share/top")
--- lib/util/cmake/copy_registration.cmake
+++ lib/util/cmake/copy_registration.cmake
@@ -1,25 +1,18 @@
 function(register_build_directory_copy dir)
-	set(_REGISTER_DIR_COPY "${_REGISTER_DIR_COPY}" CACHE INTERNAL "")
+	# We register the copy dir in a not-so-hidden property that will be used later on
 	set_property(GLOBAL PROPERTY _REGISTER_DIR_COPY_property "${_REGISTER_DIR_COPY_property};${dir}")
-	#set(ENV{_REGISTER_DIR_COPY} "$ENV{_REGISTER_DIR_COPY};${dir}")
-	list(APPEND _REGISTER_DIR_COPY ${dir})
-	get_property(v GLOBAL PROPERTY _REGISTER_DIR_COPY_property)
-	message("register_build_directory_copy -  input : <${dir}>\nEnv var : <$ENV{_REGISTER_DIR_COPY}>\nglobal var : <${_REGISTER_DIR_COPY}>\nproperty var : <${v}>")
-	#write_file("./some_file.txt" "${dir}")
+
 endfunction()
 
 function(copy_directory_data target dest)
+
 	set(num 0)
-	#message("copy_directory_data - Env var : <$ENV{_REGISTER_DIR_COPY}>")
-	#message("copy_directory_data - global var : <${_REGISTER_DIR_COPY}>")
-	get_property(v GLOBAL PROPERTY _REGISTER_DIR_COPY_property)
-	message("copy_directory_data - property var : <${v}>")
-
-	set(hopefully_a_list $ENV{_REGISTER_DIR_COPY})
-	list(SORT hopefully_a_list)
-
-	#foreach(dir_it IN hopefully_a_list)
-	#	add_custom_target("${target}_directory_copy${num}" ALL COMMAND ${CMAKE_COMMAND} -E copy_if_different dir_it dest)
-	#	set(num  (num + 1))
-	#endforeach()
+	set(property_var)
+	get_property(property_var GLOBAL PROPERTY _REGISTER_DIR_COPY_property)
+	list(POP_FRONT property_var)
+	foreach(dir_it IN LISTS  property_var)
+		message("Looping on <${dir_it}>")
+		add_custom_target("${target}_directory_copy${num}" ALL COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different ${dir_it} $<TARGET_FILE_DIR:${target}>/lol )
+		set(num  (num + 1))
+	endforeach()
 endfunction()
