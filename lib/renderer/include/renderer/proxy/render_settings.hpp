94a2bb7d0ada8cc23c11b7f1f0e28a8cda7d595f
sguionni <simon.guionniere@gmail.com>
2024-01-23 10:37:42 +0100

    Fix voxel sizes

---
+++ lib/renderer/include/renderer/proxy/render_settings.hpp
@@ -0,0 +1,13 @@
+#ifndef __VTX_RENDERER_PROXY_RENDER_SETTINGS__
+#define __VTX_RENDERER_PROXY_RENDER_SETTINGS__
+
+namespace VTX::Renderer::Proxy
+{
+	// TODO: to handle multiples render settings in the same scene.
+	struct RenderSettings
+	{
+	};
+
+} // namespace VTX::Renderer::Proxy
+
+#endif
