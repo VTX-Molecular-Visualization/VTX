5731cbb45f1a9c1a37ef909728bd93be547f3738
Valentin Guillaume <valent.guillaume@gmail.com>
2024-02-24 12:56:35 +0100

    WIP - Refactoring dev

--- lib/ui/test/cmake/library.cmake
+++ lib/ui/test/cmake/library.cmake
@@ -2,14 +2,14 @@ add_executable(vtx_ui_test "${CMAKE_CURRENT_LIST_DIR}/../src/main.cpp")
 configure_target(vtx_ui_test)
 
 if (NOT DEFINED _VTX_UI_TEST_CONAN)
-	target_link_libraries(vtx_ui_test PRIVATE vtx_util)
-	target_link_libraries(vtx_ui_test PRIVATE vtx_core)
-	target_link_libraries(vtx_ui_test PRIVATE vtx_renderer)
-	target_link_libraries(vtx_ui_test PRIVATE vtx_io)
-	target_link_libraries(vtx_ui_test PRIVATE vtx_app)
-	target_link_libraries(vtx_ui_test PRIVATE vtx_python_binding)
-	target_link_libraries(vtx_ui_test PRIVATE PyTX)
-	target_link_libraries(vtx_ui_test PRIVATE vtx_ui)
+	target_link_libraries(vtx_ui_test PUBLIC vtx_util)
+	target_link_libraries(vtx_ui_test PUBLIC vtx_core)
+	target_link_libraries(vtx_ui_test PUBLIC vtx_renderer)
+	target_link_libraries(vtx_ui_test PUBLIC vtx_io)
+	target_link_libraries(vtx_ui_test PUBLIC vtx_app)
+	target_link_libraries(vtx_ui_test PUBLIC vtx_python_binding)
+	target_link_libraries(vtx_ui_test PUBLIC PyTX)
+	target_link_libraries(vtx_ui_test PUBLIC vtx_ui)
 else()
 	target_link_libraries(vtx_ui_test PRIVATE vtx_util::vtx_util)
 	target_link_libraries(vtx_ui_test PRIVATE vtx_core::vtx_core)
@@ -24,18 +24,8 @@ endif()
 target_link_libraries(vtx_ui_test PRIVATE pybind11::embed)
 target_link_libraries(vtx_ui_test PRIVATE Catch2::Catch2WithMain)
 
-
-
-add_custom_target(
-	vtx_ui_test_copy_shader
-	ALL COMMAND
-	${CMAKE_COMMAND} -E copy_directory	
-	${DIR_SHADERS}
-	$<TARGET_FILE_DIR:vtx_ui_test>/shaders)
-add_dependencies(vtx_ui_test vtx_ui_test_copy_shader)
-
-add_custom_target(vtx_ui_copy_data ALL COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/data $<TARGET_FILE_DIR:vtx_ui_test>/data)
-add_dependencies(vtx_ui_test vtx_ui_copy_data)
+vtx_register_build_directory_copy("${CMAKE_CURRENT_LIST_DIR}/../data" "./data")
+vtx_copy_registered_data(vtx_ui_test)
 
 include(CTest)
 include(Catch)
