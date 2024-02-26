374880d2bf8a648e8f61395428b124a01d3daa43
Valentin Guillaume <valent.guillaume@gmail.com>
2024-02-26 21:54:17 +0100

    Trying to fix renderer's CI

--- lib/renderer/cmake/library.cmake
+++ lib/renderer/cmake/library.cmake
@@ -6,7 +6,7 @@ configure_target(vtx_renderer)
 set(HEADERS "")
 set(SOURCES "")
 set(SHADERS "")
-file(GLOB_RECURSE HEADERS "${CMAKE_CURRENT_LIST_DIR}/../src/include/*")
+file(GLOB_RECURSE HEADERS "${CMAKE_CURRENT_LIST_DIR}/../include/*")
 file(GLOB_RECURSE SOURCES "${CMAKE_CURRENT_LIST_DIR}/../src/*")
 file(GLOB_RECURSE SHADERS "${CMAKE_CURRENT_LIST_DIR}/../shaders/*")
 target_sources(vtx_renderer
