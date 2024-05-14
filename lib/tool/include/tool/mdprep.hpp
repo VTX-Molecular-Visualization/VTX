4fce6a8f705de009f4f6e26d1381d85bae75a9df
Valentin <valent.guillaume@gmail.com>
2024-05-14 18:20:00 +0200

    Adding mdprep button

--- lib/tool/include/tool/mdprep.hpp
+++ lib/tool/include/tool/mdprep.hpp
@@ -2,21 +2,27 @@
 #define __VTX_TOOL_MDPREP_REGISTRATION__
 
 #include "ui/core/tool_registry.hpp"
+#include "ui/qt/base_qt_tool.hpp"
 
 namespace VTX::Tool
 {
-	class ToolMdprep : public VTX::UI::Core::BaseVTXUITool
+	// Class responsible for showing a button that call the MDprep tool
+	class ToolMdprep : public VTX::UI::QT::BaseQtTool
 	{
 	  public:
-		inline static const char NAME[] { "ToolMdprep" };
+		inline static const UI::Core::ToolIdentifier TOOL_KEY = "TOOL_MDPREP";
 
 	  private:
-		inline static const VTX::UI::Core::ToolRegistry::Registration<ToolMdprep> _reg { NAME };
+		inline static const VTX::UI::Core::ToolRegistry::Registration<ToolMdprep> _reg { TOOL_KEY };
 
 	  public:
 		ToolMdprep();
 
 		void instantiateTool() override;
+
+	  private:
+		void _addButtonsInMenu();
+		void _openMdPrepWindow();
 	};
 } // namespace VTX::Tool
 
--- lib/tool/src/tool/mdprep.cpp
+++ lib/tool/src/tool/mdprep.cpp
@@ -1,9 +1,43 @@
 #include "tool/mdprep.hpp"
+#include "ui/qt/application_qt.hpp"
+#include "ui/qt/main_window.hpp"
+#include "ui/qt/tool/pytx/details/include_python_binding.hpp"
+#include "ui/qt/widget/main_menu/menu_toolblock_widget.hpp"
+#include "ui/qt/widget/main_menu/menu_toolbutton_widget.hpp"
+#include "ui/qt/widget_factory.hpp"
+#include <app/application/system/action_manager.hpp>
+#include <app/core/action/base_action.hpp>
 #include <util/logger.hpp>
 
 namespace VTX::Tool
 {
 
 	ToolMdprep::ToolMdprep() {}
-	void ToolMdprep::instantiateTool() { VTX_INFO( "Instantiate ToolMdprep" ); }
+	void ToolMdprep::instantiateTool()
+	{
+		VTX_INFO( "Instantiate ToolMdprep" );
+		_addButtonsInMenu();
+	}
+	void ToolMdprep::_addButtonsInMenu()
+	{
+		UI::Core::ToolLayoutData mainMenuLayoutData = UI::Core::ToolLayoutData();
+		mainMenuLayoutData.tabName					= "Tools";
+		mainMenuLayoutData.blockName				= "MD";
+
+		VTX::UI::QT::MainWindow * const						 mainWindow = &VTX::UI::QT::QT_APP()->getMainWindow();
+		VTX::UI::QT::Widget::MainMenu::MenuToolBlockWidget & toolBlock
+			= mainWindow->getMainMenuToolBlock( mainMenuLayoutData );
+
+		VTX::UI::QT::Widget::MainMenu::MenuToolButtonWidget * const button
+			= VTX::UI::QT::WidgetFactory::get().instantiateWidget<VTX::UI::QT::Widget::MainMenu::MenuToolButtonWidget>(
+				&toolBlock, "MdPrepButton"
+			);
+		// button->setData( "MD prep", ":/sprite/info_button.png", Qt::Orientation::Vertical );
+		button->setData( "MD prep", ":/sprite/icon_tool_mdprep_mainButton.png", Qt::Orientation::Vertical );
+		button->setTriggerAction( this, &ToolMdprep::_openMdPrepWindow );
+
+		toolBlock.pushButton( *button );
+	}
+	void ToolMdprep::_openMdPrepWindow() { VTX_INFO( "Opening ToolMdprep window" ); }
+
 } // namespace VTX::Tool
