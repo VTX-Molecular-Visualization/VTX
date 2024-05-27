f89d111da4b033bf12c4fcf167afd95378704532
Valentin <valent.guillaume@gmail.com>
2024-05-28 00:08:12 +0200

    Adding advanced form callback from basic form change

--- lib/tool/tools/mdprep/src/ui/form_data.cpp
+++ lib/tool/tools/mdprep/src/ui/form_data.cpp
@@ -15,4 +15,16 @@ namespace VTX::Tool::Mdprep::ui
 		}
 	}
 
+	const double  multiplierPicoSeconds( const E_MD_DURATION_UNIT & p_ ) noexcept
+	{
+		switch ( p_ )
+		{
+		case E_MD_DURATION_UNIT::femtoSeconds: return 1e3;
+		case E_MD_DURATION_UNIT::picoSeconds: return 1e0;
+		case E_MD_DURATION_UNIT::nanoSeconds: return 1e3;
+		case E_MD_DURATION_UNIT::microSeconds: return 1e6;
+		default: return 1.0;
+		}
+	}
+
 } // namespace VTX::Tool::Mdprep::ui
