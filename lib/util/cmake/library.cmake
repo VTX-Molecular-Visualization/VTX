d22ea6aaeb63e44046f6287af4a47081624b0017
sguionni <simon.guionniere@gmail.com>
2024-07-17 01:28:42 +0200

    Fix CI

--- lib/util/cmake/library.cmake
+++ lib/util/cmake/library.cmake
@@ -10,19 +10,11 @@ target_sources(vtx_util
 	PRIVATE ${SOURCES}
 	PUBLIC FILE_SET public_headers TYPE HEADERS BASE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../include" FILES ${HEADERS})
 
-if (NOT DEFINED _VTX_UTIL_CONAN)
-	target_link_libraries(vtx_util glm::glm)
-	target_link_libraries(vtx_util spdlog::spdlog)
-	target_link_libraries(vtx_util nlohmann_json::nlohmann_json)
-	target_link_libraries(vtx_util magic_enum::magic_enum)
-	target_link_libraries(vtx_util cpr::cpr)
-else()
-	target_link_libraries(vtx_util PRIVATE glm::glm)
-	target_link_libraries(vtx_util PRIVATE spdlog::spdlog)
-	target_link_libraries(vtx_util PRIVATE nlohmann_json::nlohmann_json)
-	target_link_libraries(vtx_util PRIVATE magic_enum::magic_enum)
-	target_link_libraries(vtx_util PRIVATE cpr::cpr)	
-endif()
+target_link_libraries(vtx_util glm::glm)
+target_link_libraries(vtx_util spdlog::spdlog)
+target_link_libraries(vtx_util nlohmann_json::nlohmann_json)
+target_link_libraries(vtx_util magic_enum::magic_enum)
+target_link_libraries(vtx_util cpr::cpr)
 
 # Tests.
 file(GLOB_RECURSE TESTS "${CMAKE_CURRENT_LIST_DIR}/../test/*")
