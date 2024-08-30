4b1b4c99288178bec1d5ccce9c3d9d479050e58a
Valentin <valent.guillaume@gmail.com>
2024-08-30 12:30:02 +0200

    Attempt to fix MdPrep CI

--- tool/mdprep/conanfile.py
+++ tool/mdprep/conanfile.py
@@ -18,7 +18,7 @@ class VTXToolMdprepRecipe(ConanFile):
     
     generators = "CMakeDeps", "CMakeToolchain"
     
-    exports_sources = "CMakeLists.txt", "src/*", "include/*", "cmake/*", "test/*"
+    exports_sources = "CMakeLists.txt", "src/*", "include/*", "cmake/*", "test/*", "data/*"
 
      
     def requirements(self):
