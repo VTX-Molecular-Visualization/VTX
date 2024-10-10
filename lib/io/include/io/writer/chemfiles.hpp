036c86685204184d7d563c1addeb8df1815a3a5d
Valentin <valent.guillaume@gmail.com>
2024-10-10 18:28:50 +0200

    WIP - writing chemfiles writer

--- lib/io/include/io/writer/chemfiles.hpp
+++ lib/io/include/io/writer/chemfiles.hpp
@@ -1,13 +1,29 @@
 #ifndef __VTX_IO_WRITER_CHEMFILES__
 #define __VTX_IO_WRITER_CHEMFILES__
 
-#include "io/util/chemfiles.hpp"
 #include <forward_list>
 #include <util/types.hpp>
 #include <variant>
 
 namespace VTX::IO::Writer
 {
+
+	enum class E_BOND_ORDER
+	{
+		unknown,
+		single,
+		doubl,
+		triple,
+		quadruple,
+		quintuple,
+		down,
+		up,
+		dative_L,
+		dative_R,
+		amide,
+		aromatic,
+	};
+
 	/**
 	 * @brief Property that can be attached to various components of the system
 	 */
@@ -40,27 +56,28 @@ namespace VTX::IO::Writer
 	 */
 	struct ChainInfo
 	{
-		uint64_t	id = 0xffffffffffffffff;
+		std::string id;
 		std::string symbol;
 	};
 
+	struct _AtomInfo;
 	/**
-	 * @brief Allows to configure an atom to be written. Atoms are provided by the System.
+	 * @brief Atoms are provided by the System.
 	 */
 	class Atom
 	{
 	  public:
 		/**
-		 * @brief Set the coordinates of the atom.
+		 * @brief You need to be the chosen one to use the function to its full power.
+		 * @param
 		 */
-		void set( AtomCoordinates ) noexcept;
+		void get( _AtomInfo & ) noexcept;
 	};
 
 	/**
-	 * @brief Allows to configure a chain to be written. The user is supposed to provide atoms to form the chain. Chains
-	 * are provided by the System.
+	 * @brief Allows to configure a residue to be written. Residues can be added atoms and properties.
 	 */
-	class Chain
+	class Residue
 	{
 	  public:
 		/**
@@ -75,18 +92,30 @@ namespace VTX::IO::Writer
 	};
 
 	/**
-	 * @brief Allow to configure one frame of the system. A frame is defined by a set of all atoms coordinates. If a
+	 * @brief Allows to configure a chain to be written.
+	 */
+	class Chain
+	{
+	  public:
+		/**
+		 * @brief Inform the chain that the input residue is a part of it.
+		 */
+		void add( Residue & ) noexcept;
+	};
+
+	/**
+	 * @brief Allow to configure one frame of the system. A frame is defined by a set of coordinates for all atoms. If a
 	 * simple structure file is written, only one frame will suffice.
 	 */
 	class Frame
 	{
 	  public:
-		void add( AtomId, AtomCoordinates ) noexcept;
+		void set( AtomId, AtomCoordinates ) noexcept;
 	};
 
 	/**
-	 * @brief Allows to configure a frame to be written by Chemfiles. Be carefull : any instance is invalidated if the
-	 * parent Chemmfiles object is moved away.
+	 * @brief Responsible for creating objects to configure the chemfiles system. Be carefull : any instance is
+	 * invalidated if the parent Chemmfiles object is moved away.
 	 */
 	class System
 	{
@@ -97,32 +126,80 @@ namespace VTX::IO::Writer
 		void set( Property ) noexcept;
 
 		/**
-		 * @brief Create a new atom in this frame
-		 * @return the new Atom
+		 * @brief  Create a new atom in this frame
+		 * @param  Id of the atom. Used as an external key to indentify the atom afterward.
 		 */
 		Atom newAtom( AtomId ) noexcept;
 
 		/**
-		 * @brief Create a new chain in this frame
-		 * @param
-		 * @return
+		 * @brief  Bind two atoms together. Atoms are identified by the external key provided during their creation.
+		 */
+		void bind( const AtomId &, const AtomId &, const E_BOND_ORDER & );
+
+		/**
+		 * @brief Create a new chain in this frame.
 		 */
 		Chain newChain( ChainInfo ) noexcept;
+
+		/**
+		 * @brief Create a new residue in this frame.
+		 */
+		Residue newResidue() noexcept;
+
+		/**
+		 * @brief Create a new frame. A healthy system should get at least one frame.
+		 */
+		Frame newFrame() noexcept;
 	};
 
 	/**
-	 * @brief Allows to write a file containing molecule's structural information using the Chemfiles library
+	 * @brief Allows to write a file containing molecule's structural information using the Chemfiles library. Be
+	 * careful : if an instance of this class is moved, any system instance currently used will be invalidated.
 	 */
 	class Chemfiles
 	{
 	  public:
+		enum class E_FILE_FORMATS
+		{
+			none,
+			pdb,
+			mmcif,
+			mmtf,
+			molden,
+			cif,
+			gro,
+			mol2,
+			sdf,
+			smi,
+			xyz,
+			cml,
+			cssr,
+			nc,
+			dcd,
+			lammpstrj,
+			arc,
+			trr,
+			xtc,
+			tng,
+			trj,
+			COUNT
+		};
+
 		Chemfiles() = default;
 
 		/**
 		 * @brief Actual writing happens at object destruction.
 		 */
 		~Chemfiles();
+
+		/**
+		 * @brief Move this object around. Invalidate the System.
+		 */
 		Chemfiles( Chemfiles && );
+
+		/**
+		 * @brief Move this object around. Invalidate the System.
+		 */
 		Chemfiles & operator=( Chemfiles && );
 		Chemfiles( const Chemfiles & )			   = delete;
 		Chemfiles & operator=( const Chemfiles & ) = delete;
@@ -136,13 +213,14 @@ namespace VTX::IO::Writer
 		 * @brief Tells the writer in which format to write structural data. If left empty or set to none, the format
 		 * will be guessed from the file's extension.
 		 */
-		void setWriteFormat( Util::Chemfiles::E_FILE_FORMATS ) noexcept;
+		void setWriteFormat( E_FILE_FORMATS ) noexcept;
 
 		/**
-		 * @brief Create the system that will be written eventually
+		 * @brief Create/retrieve the system that will be written eventually. The returned system will be invalidated is
+		 * this object is moved.
 		 * @return The system to configure
 		 */
-		System createSystem() noexcept;
+		System system() noexcept;
 
 	  private:
 		struct _Chain
@@ -155,10 +233,11 @@ namespace VTX::IO::Writer
 		using FrameCollection = std::forward_list<_Frame>; // Used for constant time insertion and memory efficiency
 		struct _Data // Here so move ctor/assignement are easier to write and maintain
 		{
-			FilePath						dest;
-			Util::Chemfiles::E_FILE_FORMATS fileFormat = Util::Chemfiles::E_FILE_FORMATS::none;
-			FrameCollection					frames;
-		} _;
+			FilePath		dest;
+			E_FILE_FORMATS	fileFormat = E_FILE_FORMATS::none;
+			FrameCollection frames;
+			ChainCollection chains;
+		} _; // best attribute name ever
 	};
 } // namespace VTX::IO::Writer
 #endif
--- lib/io/src/io/writer/chemfiles.cpp
+++ lib/io/src/io/writer/chemfiles.cpp
@@ -2,12 +2,9 @@
 
 namespace VTX::IO::Writer
 {
-	void Chemfiles::setWriteDestination( FilePath p_ ) noexcept { _dest = std::move( p_ ); }
-
-	void Chemfiles::setWriteFormat( Util::Chemfiles::E_STRUCT_FILE_FORMAT p_ ) noexcept
+	struct _AtomInfo
 	{
-		_fileFormat = std::move( p_ );
-	}
 
+	};
 
 } // namespace VTX::IO::Writer
