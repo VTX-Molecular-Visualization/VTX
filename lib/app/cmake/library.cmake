b90840c7c6af8cca13c3950e9b36e0cdcfe501bd
Valentin <valent.guillaume@gmail.com>
2024-11-18 21:09:00 +0100

    Quick patch to make app_test work in dev mode

--- lib/app/cmake/library.cmake
+++ lib/app/cmake/library.cmake
@@ -10,9 +10,12 @@ target_sources(vtx_app
 
 # Tests.
 # I think OpenGL from {vtx_app->vtx_renderer} prevents us to link the target vtx_renderer_no_opengl normally 
+# So instead of app_test linking against app, we compile source from app directly into app_test. 
+# This require for libraires that are private to app to be linked with app_test as well.
+# Moreover, we need to keep in mind that we are thus testing the code, not the project.
 
 file(GLOB_RECURSE SOURCES_TEST "${CMAKE_CURRENT_LIST_DIR}/../test/*")
-add_executable(vtx_app_test "${SOURCES_TEST}")
+add_executable(vtx_app_test "${SOURCES_TEST};${SOURCES}")
 target_include_directories(vtx_app_test PRIVATE "${CMAKE_CURRENT_LIST_DIR}/../include")
 
 configure_target(vtx_app_test)
@@ -38,8 +41,8 @@ else()
 endif()
 
 target_link_libraries(vtx_app PUBLIC EnTT::EnTT)
-#target_link_libraries(vtx_app_test PUBLIC EnTT::EnTT)
-target_link_libraries(vtx_app_test PRIVATE vtx_app)
+target_link_libraries(vtx_app_test PUBLIC EnTT::EnTT)
+#target_link_libraries(vtx_app_test PRIVATE vtx_app)
 target_link_libraries(vtx_app_test PRIVATE Catch2::Catch2WithMain)
 
 vtx_register_build_directory_copy("${CMAKE_CURRENT_LIST_DIR}/../data" "./data")
