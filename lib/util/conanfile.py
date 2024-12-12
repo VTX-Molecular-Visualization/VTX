a85877cba340932e756407e55ef7f70ff520425f
sguionni <simon.guionniere@gmail.com>
2024-12-12 19:38:16 +0100

    Update libs

--- lib/util/conanfile.py
+++ lib/util/conanfile.py
@@ -19,12 +19,12 @@ class VTXUtilRecipe(ConanFile):
         
     def requirements(self):
         self.requires("glm/0.9.9.8", transitive_headers=True)
-        self.requires("spdlog/1.14.1", transitive_headers=True)
+        self.requires("spdlog/1.15.0", transitive_headers=True)
         self.requires("nlohmann_json/3.11.3", transitive_headers=True)
-        self.requires("magic_enum/0.9.6", transitive_headers=True)
-        self.requires("cpr/1.10.5")
+        self.requires("magic_enum/0.9.7", transitive_headers=True)
+        self.requires("cpr/1.11.1")
         self.requires("stb/cci.20240531")
-        self.requires("catch2/3.7.0")
+        self.requires("catch2/3.7.1")
         
     def config_options(self):
         if self.settings.os == "Windows":
