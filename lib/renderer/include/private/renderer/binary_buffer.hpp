89c7145dc0791c1ef682ed2550f91d48032107ea
sguionni <simon.guionniere@gmail.com>
2025-01-15 02:34:25 +0100

    Fix gcc

--- lib/renderer/include/private/renderer/binary_buffer.hpp
+++ lib/renderer/include/private/renderer/binary_buffer.hpp
@@ -12,48 +12,6 @@ namespace VTX::Renderer
 	 */
 	class BinaryBuffer
 	{
-	  public:
-		/**
-		 * @brief Write data adn padding at the current offset.
-		 */
-		template<typename T>
-		void write( const T & p_value )
-		{
-			assert( _opened );
-
-			const size_t size	   = sizeof( T );
-			const size_t alignment = TypeAlignment<T>::value;
-			const size_t padding   = ( alignment - ( _offset % alignment ) ) % alignment;
-
-			_data.insert( _data.end(), padding + size, 0 );
-
-			_offset += padding;
-			std::memcpy( &_data[ _offset ], &p_value, size );
-			_offset += size;
-		}
-
-		/**
-		 * @brief Align to 16 bytes.
-		 */
-		void close()
-		{
-			assert( _opened );
-
-			const size_t padding = ( 16 - ( _offset % 16 ) ) % 16;
-			_data.insert( _data.end(), padding, 0 );
-
-			_opened = false;
-		}
-
-		inline const void * data() const { return _data.data(); }
-		inline size_t		size() const { return _data.size(); }
-		inline size_t		offset() const { return _offset; }
-
-	  private:
-		std::vector<uint8_t> _data;
-		bool				 _opened = true;
-		size_t				 _offset = 0;
-
 		template<typename T>
 		struct TypeAlignment;
 
@@ -110,6 +68,48 @@ namespace VTX::Renderer
 		{
 			static constexpr size_t value = 16;
 		};
+
+	  public:
+		/**
+		 * @brief Write data adn padding at the current offset.
+		 */
+		template<typename T>
+		void write( const T & p_value )
+		{
+			assert( _opened );
+
+			const size_t size	   = sizeof( T );
+			const size_t alignment = TypeAlignment<T>::value;
+			const size_t padding   = ( alignment - ( _offset % alignment ) ) % alignment;
+
+			_data.insert( _data.end(), padding + size, 0 );
+
+			_offset += padding;
+			std::memcpy( &_data[ _offset ], &p_value, size );
+			_offset += size;
+		}
+
+		/**
+		 * @brief Align to 16 bytes.
+		 */
+		void close()
+		{
+			assert( _opened );
+
+			const size_t padding = ( 16 - ( _offset % 16 ) ) % 16;
+			_data.insert( _data.end(), padding, 0 );
+
+			_opened = false;
+		}
+
+		inline const void * data() const { return _data.data(); }
+		inline size_t		size() const { return _data.size(); }
+		inline size_t		offset() const { return _offset; }
+
+	  private:
+		std::vector<uint8_t> _data;
+		bool				 _opened = true;
+		size_t				 _offset = 0;
 	};
 
 } // namespace VTX::Renderer
