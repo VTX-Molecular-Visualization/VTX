af15b101891c13dbf72e4b7861b3833717e90ceb
Valentin <valent.guillaume@gmail.com>
2025-04-09 16:56:08 +0200

    WIP - App->Python - adding helper header

---
+++ lib/python_binding/include/python_binding/api/api.hpp
@@ -0,0 +1,10 @@
+#ifndef __VTX_PYTHON_API_API__
+
+#include <concepts>
+//
+#include "python_binding/api/atom.hpp"
+#include "python_binding/api/chain.hpp"
+#include "python_binding/api/residue.hpp"
+#include "python_binding/api/system.hpp"
+
+#endif
