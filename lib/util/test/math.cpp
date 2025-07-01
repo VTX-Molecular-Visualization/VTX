e9a331ccd5eedc88ae8f89799572a974426b240c
Valentin <valent.guillaume@gmail.com>
2025-07-01 17:24:25 +0200

    Adding torsion testing

---
+++ lib/util/test/math.cpp
@@ -0,0 +1,26 @@
+#include <catch2/catch_test_macros.hpp>
+#include <util/math.hpp>
+
+namespace
+{
+	bool closeEnough( const float & v1, const float & v2 ) { return std::abs( v1 - v2 ) < 0.001f; }
+	bool checkTorsion(
+		const VTX::Vec3f & p1,
+		const VTX::Vec3f & p2,
+		const VTX::Vec3f & p3,
+		const VTX::Vec3f & p4,
+		const float &	   expectedValue
+	)
+	{
+		return closeEnough( VTX::Util::Math::torsionalAngle( p1, p2, p3, p4 ), expectedValue );
+	}
+} // namespace
+
+TEST_CASE( "Util::Math::torsionalAngle", "[math]" )
+{
+	using namespace VTX;
+
+	CHECK( checkTorsion( { -1, 0, 0 }, { 0, 0, 0 }, { 0, 1, 0 }, { 0, 1, 1 }, VTX::PI_2f ) );
+	CHECK( checkTorsion( { -1, 0, 0 }, { 0, 0, 0 }, { 0, 1, 0 }, { -1, 1, 1 }, VTX::PI_2f / 2.f ) );
+	CHECK( checkTorsion( { -1, 0, 0 }, { 0, 0, 0 }, { 0, 1, 0 }, { -1, 1, -1 }, -( VTX::PI_2f / 2.f ) ) );
+}
