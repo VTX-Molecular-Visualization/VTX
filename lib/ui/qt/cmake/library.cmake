7fb232b0a60e03423d14f32ccd3b7fa2a560763c
sguionni <sguionni@pm.me>
2025-07-09 02:38:27 +0200

    Clean

--- lib/ui/qt/cmake/library.cmake
+++ lib/ui/qt/cmake/library.cmake
@@ -4,11 +4,8 @@ configure_qt()
 add_library(vtx_ui_qt)
 configure_target(vtx_ui_qt)
 
-set(QT_RESOURCES_PATH ${CMAKE_CURRENT_LIST_DIR}/../asset)
-
 file(GLOB_RECURSE HEADERS ${CMAKE_CURRENT_LIST_DIR}/../include/*)
 file(GLOB_RECURSE SOURCES ${CMAKE_CURRENT_LIST_DIR}/../src/*)
-file(GLOB_RECURSE QT_RESOURCES ${QT_RESOURCES_PATH}/*)
 target_sources(vtx_ui_qt
 	PRIVATE ${SOURCES}
 	PUBLIC FILE_SET public_headers TYPE HEADERS BASE_DIRS ${CMAKE_CURRENT_LIST_DIR}/../include FILES ${HEADERS})
