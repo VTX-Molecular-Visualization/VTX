e05ae25b84c473bef5fd87f4d4c35ede1991f85b
sguionni <sguionni@pm.me>
2025-07-16 01:21:56 +0200

    Missing conanfile.py

--- lib/ui/qt/conanfile.py
+++ lib/ui/qt/conanfile.py
@@ -22,7 +22,7 @@ class VTXUiQtRecipe(ConanFile):
         self.requires("vtx_renderer/1.0")
         self.requires("vtx_core/1.0")
         self.requires("vtx_python_binding/1.0", transitive_headers=True)
-        self.requires("qt/6.6.3", transitive_headers=True)
+        self.requires("qt/6.8.3", transitive_headers=True)
         self.requires("entt/3.14.0", transitive_headers=True) # TODO : see <lib/python_binding/cmake/library.cmake> for details
         
     def config_options(self):
