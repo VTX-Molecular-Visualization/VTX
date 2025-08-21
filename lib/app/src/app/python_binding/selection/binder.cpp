64e67a10ee82cc4b2f36ab27f20b79f09afb188e
Valentin <valent.guillaume@gmail.com>
2025-08-21 16:05:27 +0200

    Try to fix a segfault

--- lib/app/src/app/python_binding/selection/binder.cpp
+++ lib/app/src/app/python_binding/selection/binder.cpp
@@ -52,7 +52,14 @@ namespace VTX::App::PythonBinding::Selection
 				&VTX::App::PythonBinding::Selection::SelectionWrapper::remove,
 				pybind11::return_value_policy::reference
 			)
-			.def( "save", &VTX::App::PythonBinding::Selection::SelectionWrapper::save )
+			.def(
+				"save",
+				&VTX::App::PythonBinding::Selection::SelectionWrapper::save,
+				pybind11::keep_alive<1, 2>(
+				) /* I suspect that a segfault sometimes occurs here. This might be due to the object being garbage
+					 collected a the wrong time. So here I try to prevent these segfaults. It is difficult to test as
+					 these occurances are quite random  */
+			)
 
 			// I don't like the 4 following definition but the "implicitly_convertible" pybind function doesn't work
 			// here and is seems that having the method signature conversion makes python garbage the memory while we
