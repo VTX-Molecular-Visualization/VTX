a923a78803f72e5a0cc201d8ea6ee8f786c3bf09
sguionni <sguionni@pm.me>
2025-11-14 00:09:40 +0100

    Fix

--- lib/ui/qt/src/ui/qt/menu/theme.cpp
+++ lib/ui/qt/src/ui/qt/menu/theme.cpp
@@ -15,9 +15,8 @@ namespace VTX::UI::QT::Menu
 	Theme::Theme( QWidget * p_parent ) : BaseWidget( "Theme", p_parent )
 	{
 		auto * system = addAction<Action::Theme::System>();
-		addSeparator();
-		auto * light = addAction<Action::Theme::Light>();
-		auto * dark	 = addAction<Action::Theme::Dark>();
+		auto * light  = addAction<Action::Theme::Light>();
+		auto * dark	  = addAction<Action::Theme::Dark>();
 		addSeparator();
 		connect( addAction<Action::Theme::ResetLayout>(), &QAction::triggered, this, &Theme::_resetLayout );
 
@@ -32,7 +31,7 @@ namespace VTX::UI::QT::Menu
 			switch ( theme )
 			{
 			case Style::E_THEME::SYSTEM: system->setChecked( true ); break;
-			case Style::E_THEME::LIGHT: dark->setChecked( true ); break;
+			case Style::E_THEME::LIGHT: light->setChecked( true ); break;
 			case Style::E_THEME::DARK: dark->setChecked( true ); break;
 			default: break;
 			}
