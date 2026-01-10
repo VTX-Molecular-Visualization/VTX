ee9a14e033f6b76980f027235e1207e27e241f9d
sguionni <sguionni@pm.me>
2026-01-10 20:49:06 +0100

    Finally

--- lib/app/cmake/library.cmake
+++ lib/app/cmake/library.cmake
@@ -49,7 +49,7 @@ if(WIN32)
 	set(VELOPACK_LIB "${CMAKE_CURRENT_LIST_DIR}/../vendor/velopack/lib-static/velopack_libc_win_x64_msvc.lib")
 	target_link_libraries(vtx_app PRIVATE ntdll)
 else()
-	set(VELOPACK_LIB "${CMAKE_CURRENT_LIST_DIR}/../vendor/velopack/lib-static/velopack_libc_linux_x64_gnu.a")
+	set(VELOPACK_LIB "${CMAKE_CURRENT_LIST_DIR}/../vendor/velopack/lib-static/libvelopack_libc_linux_x64_gnu.a")
 endif()
 target_link_libraries(vtx_app PRIVATE ${VELOPACK_LIB})
 
