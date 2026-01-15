3a73f9f62c2c0a46c985bbc2a2535992cd79d3bc
sguionni <sguionni@pm.me>
2026-01-15 18:44:52 +0100

    Missing file

--- lib/python_binding/conanfile.py
+++ lib/python_binding/conanfile.py
@@ -126,7 +126,7 @@ class VTXPythonBindingRecipe(ConanFile):
         self.requires("vtx_io/1.0")
         self.requires("pybind11/2.13.6", transitive_headers=True)
         self.requires("catch2/3.11.0")
-        self.requires("cpython/3.9.19") # v >= 3.10 not working with msvc compiler so far
+        self.requires("cpython/3.12.7") # v >= 3.10 not working with msvc compiler so far
         
     def config_options(self):
         if self.settings.os == "Windows":
