a10e50d95c088fc46306ea15f8239680d3754667
sguionni <sguionni@pm.me>
2026-02-07 17:22:03 +0100

    Representations menu

--- lib/ui/qt/include/ui/qt/menu/representation.hpp
+++ lib/ui/qt/include/ui/qt/menu/representation.hpp
@@ -2,7 +2,9 @@
 #define __VTX_UI_QT_MENU_REPRESENTATION__
 
 #include <QMenu>
+#include <app/preset/name.hpp>
 #include <optional>
+#include <renderer/representation.hpp>
 
 namespace VTX::UI::QT::Menu
 {
@@ -13,6 +15,21 @@ namespace VTX::UI::QT::Menu
 		Representation( QWidget * p_parent, const std::optional<App::ECS::Entity> p_representation = std::nullopt ) :
 			BaseWidget( p_parent )
 		{
+			setTitle( "Representation" );
+
+			auto & reg	= App::REG();
+			auto   view = reg.view<Renderer::Representation>();
+
+			for ( const auto ent : view )
+			{
+				const auto & representation = view.get<Renderer::Representation>( ent );
+				const auto & name			= App::REG().get<App::Preset::Name>( ent );
+
+				QAction * a = QMenu::addAction( QString::fromStdString( name.name ) );
+				a->setCheckable( true );
+				a->setChecked( p_representation && *p_representation == ent );
+				a->setData( QVariant::fromValue( ent ) );
+			}
 		}
 	};
 
