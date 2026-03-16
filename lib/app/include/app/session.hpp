4ab7c5731cb23978e35217cb211ead4fba579554
sguionni <sguionni@pm.me>
2026-03-16 19:54:52 +0100

    CD url

--- lib/app/include/app/session.hpp
+++ lib/app/include/app/session.hpp
@@ -18,7 +18,7 @@ namespace VTX::App
 	constexpr std::string_view ORGANIZATION_DOMAIN		= "vtx.drugdesign.fr";
 
 	constexpr std::string_view UPDATE_URL
-		= "https://github.com/VTX-Molecular-Visualization/VTX_release/releases/latest/";
+		= "https://github.com/VTX-Molecular-Visualization/VTX_release/releases/latest/download";
 
 	/**
 	 * @brief Runtime session.
