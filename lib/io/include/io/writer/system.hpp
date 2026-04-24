a4a26a12d7644ab3463190cd128786fc325700d7
Valentin <valent.guillaume@gmail.com>
2026-04-24 17:46:58 +0200

    WIP - fixing trajectory write

--- lib/io/include/io/writer/system.hpp
+++ lib/io/include/io/writer/system.hpp
@@ -19,14 +19,90 @@ namespace VTX::IO::Writer
 	 */
 	inline bool		 returnTrue( const VTX::Core::Struct::Topology &, const size_t & ) noexcept { return true; }
 	const AtomFilter g_takeAllAtoms = AtomFilter( &returnTrue ); // default argument for the write atom filter
+
+	/**
+	 * @brief Class responsible for providing atom positions of each frame of a trajectory
+	 */
+	class TrajectoryFrameGetter
+	{
+	  public:
+		/**
+		 * @brief Return number of available frames
+		 * @return
+		 */
+		uint frameCount() const;
+
+		/**
+		 * @brief Returns the n-th frame
+		 * @return
+		 */
+		std::span<const Vec3f> getAtomPositions( const uint & ) const;
+		std::span<const Vec3f> getCurrentAtomPositions() const;
+
+	  private:
+		struct _interface
+		{
+			virtual ~_interface()												  = default;
+			virtual uint				   frameCount() const					  = 0;
+			virtual std::span<const Vec3f> getAtomPositions( const uint & ) const = 0;
+			virtual std::span<const Vec3f> getCurrentAtomPositions() const		  = 0;
+		};
+		struct _dummy
+		{
+		};
+		template<typename T>
+		struct _wrapper final : public _interface
+		{
+			_wrapper( T && p_obj ) : _obj( std::forward<T>( p_obj ) ) {}
+			virtual uint frameCount() const override
+			{
+				if constexpr ( std::same_as<T, _dummy> )
+					return 0;
+				else
+				{
+					return _obj.frameCount();
+				}
+			}
+			virtual std::span<const Vec3f> getAtomPositions( const uint & p_index ) const override
+			{
+				if constexpr ( std::same_as<T, _dummy> )
+					return {};
+				else
+				{
+					return _obj.getAtomPositions( p_index );
+				}
+			}
+			virtual std::span<const Vec3f> getCurrentAtomPositions() const override
+			{
+				if constexpr ( std::same_as<T, _dummy> )
+					return {};
+				else
+				{
+					return _obj.getCurrentAtomPositions();
+				}
+			}
+
+		  private:
+			T _obj;
+		};
+		std::unique_ptr<_interface> _ptr { new _wrapper<_dummy>( _dummy() ) };
+
+	  public:
+		template<typename T>
+		TrajectoryFrameGetter( T && p_ ) : _ptr( new _wrapper<T>( std::forward<T>( p_ ) ) )
+		{
+		}
+	};
+
 	/**
 	 * @brief Class responsible for writing structural data from a system
 	 */
 	struct WriteArgs
 	{
 		FilePath							destination;
-		E_FILE_FORMATS						format	   = E_FILE_FORMATS::none;
-		const VTX::Core::Struct::Topology * system	   = nullptr;
+		E_FILE_FORMATS						format	 = E_FILE_FORMATS::none;
+		const VTX::Core::Struct::Topology * topology = nullptr;
+		TrajectoryFrameGetter				trajectory;
 		AtomFilter							atomFilter = g_takeAllAtoms;
 		E_WRITE_TYPE writeType = E_WRITE_TYPE::trajectory; // Placeholder because at some point we will probably need to
 														   // write docking results and stuff
