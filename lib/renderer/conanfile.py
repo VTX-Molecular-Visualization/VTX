7fc8f8cc6d71727b6e9b69ec19e41e90cd1ac523
sguionni <sguionni@pm.me>
2026-05-18 04:52:52 +0200

    Try to fix graph dependencies

--- lib/renderer/conanfile.py
+++ lib/renderer/conanfile.py
@@ -28,6 +28,7 @@ class VTXRendererRecipe(ConanFile):
         self.requires("vtx_core/1.0")
         self.requires("catch2/3.14.0")
         if self.settings.os == "Linux":
+            self.requires("libffi/3.4.8", force=True)
             self.requires("wayland/1.24.0")
             
     def system_requirements(self):
