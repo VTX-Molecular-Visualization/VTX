c98625bd3cbb1f5693004174d7230e28f22b9b90
sguionni <sguionni@pm.me>
2026-06-04 19:19:21 +0200

    steak frites

--- tool/example/cmake/library.cmake
+++ tool/example/cmake/library.cmake
@@ -29,7 +29,7 @@ target_link_libraries(vtx_tool_example_test PRIVATE vtx_app::vtx_app)
 
 # Link tests.
 target_link_libraries(vtx_tool_example_test PRIVATE vtx_tool_example::vtx_tool_example)
-target_link_libraries(vtx_tool_example_test PRIVATE Catch2::Catch2WithMain)
+target_link_libraries(vtx_tool_example_test PRIVATE Catch2::Catch2)
 
 if (VTX_RENDERER)
 	vtx_link_cuda(vtx_tool_example_test)
