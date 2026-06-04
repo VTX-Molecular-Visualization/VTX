d82b9b93baabff6ab8361bcc56dce54014427dde
Valentin <valent.guillaume@gmail.com>
2026-06-04 11:21:53 +0200

    Narrowing test to system test for ci debugging

--- tool/mdprep/conanfile.py
+++ tool/mdprep/conanfile.py
@@ -118,7 +118,7 @@ class VTXToolMdprepRecipe(ConanFile):
         cmake.configure()
         cmake.build()
         if self.options.test == True:
-            cmake.ctest(["--output-on-failure"])
+            cmake.ctest(["[SystemTester]", "--output-on-failure"])
         
     def package(self):
         cmake = CMake(self)
