894b6e8055c9c35985d55f936c53419d72d23af2
Valentin <valent.guillaume@gmail.com>
2026-09-10 21:13:11 +0200

    WIP - CEAlign

--- tool/structalign/src/tool/structalign/backend/align.cpp
+++ tool/structalign/src/tool/structalign/backend/align.cpp
@@ -1,10 +1,14 @@
 #include <tool/structalign/backend/align.hpp>
+#include <util/math.hpp>
+#include <util/math/matrix.hpp>
 
 namespace VTX::Tool::Structalign::backend
 {
 
 	namespace
 	{
+		using Path = std::vector<std::pair<VTX::Index, VTX::Index>>;
+
 		Vec3f computeResidueCenterOfMass( const std::span<const Vec3f> p_atomPositions )
 		{
 			Vec3f result { 0.f, 0.f, 0.f };
@@ -13,7 +17,7 @@ namespace VTX::Tool::Structalign::backend
 				result += it_pos;
 			}
 			result /= p_atomPositions.size();
-			return result
+			return result;
 		}
 
 		std::vector<Vec3f> generateResiduePositionsVector( const Structure & p_system )
@@ -62,6 +66,68 @@ namespace VTX::Tool::Structalign::backend
 
 			return residuePositionsVector;
 		}
+
+		Util::Math::Matrix<float> computeDistanceMatrix( const std::vector<Vec3f> & p_positions )
+		{
+			// VG : Need to check if the distance formula used is the (7) from page 2
+
+			Util::Math::Matrix<float> distanceMatrix
+				= Util::Math::Matrix<float>( p_positions.size(), p_positions.size() );
+
+			for ( size_t iRow = 0; iRow < p_positions.size(); iRow++ )
+			{
+				for ( size_t iCol = iRow; iCol < p_positions.size(); iCol++ )
+				{
+					const float dist = VTX::Util::Math::distance( p_positions[ iRow ], p_positions[ iCol ] );
+
+					distanceMatrix.set( iRow, iCol, dist );
+					distanceMatrix.set( iCol, iRow, dist );
+				}
+			}
+
+			return distanceMatrix;
+		}
+
+		VTX::Util::Math::Matrix<float> computeScoreMatrix(
+			const VTX::Util::Math::Matrix<float> & p_distanceMatrix1,
+			const VTX::Util::Math::Matrix<float> & p_distanceMatrix2,
+			const CEAlignParameters &			   p_parameters
+		)
+		{
+			VTX::Util::Math::Matrix<float> scoreMatrix = VTX::Util::Math::Matrix<float>(
+				p_distanceMatrix1.getRowCount(), p_distanceMatrix2.getRowCount(), -1.f
+			);
+
+			const int windowSize = p_parameters.windowSize;
+
+			const int sumSize = ( windowSize - 1.f ) * ( windowSize - 2.f ) / 2.f;
+
+			const size_t windSizeMat1 = p_distanceMatrix1.getRowCount() - windowSize;
+			const size_t windSizeMat2 = p_distanceMatrix2.getRowCount() - windowSize;
+
+			for ( size_t iA = 0; iA <= windSizeMat1; iA++ )
+			{
+				for ( size_t iB = 0; iB <= windSizeMat2; iB++ )
+				{
+					float score = 0.f;
+
+					for ( size_t row = 0; row < windowSize - 2; row++ )
+					{
+						for ( size_t col = row + 2; col < windowSize; col++ )
+						{
+							score += fabsf(
+								p_distanceMatrix1.get( iA + row, iA + col )
+								- p_distanceMatrix2.get( iB + row, iB + col )
+							);
+						}
+					}
+
+					scoreMatrix.set( iA, iB, score / sumSize );
+				}
+			}
+
+			return scoreMatrix;
+		}
 	} // namespace
 
 	void align( const CEAlignParameters & p_params, CEAlignResults & p_results )
@@ -76,18 +142,18 @@ namespace VTX::Tool::Structalign::backend
 
 		correctedParameters.windowSize = std::min( p_params.windowSize, static_cast<uint32_t>( sqrt( minMolSize ) ) );
 
-		std::vector<Vec3f> staticMoleculeResiduePositions
-			= _generateResiduePositionsVector( p_staticMolecule, castedParameters );
-		std::vector<Vec3f> mobileMoleculeResiduePositions
-			= _generateResiduePositionsVector( p_mobileMolecule, castedParameters );
+		std::vector<Vec3f> staticMoleculeResiduePositions = generateResiduePositionsVector( p_params.staticStructure );
+		std::vector<Vec3f> mobileMoleculeResiduePositions = generateResiduePositionsVector( p_params.mobileStructure );
 
 		// Calculate distance matrix
-		const Math::Matrix<float> distanceMatrixStaticMol = _computeDistanceMatrix( staticMoleculeResiduePositions );
-		const Math::Matrix<float> distanceMatrixMobileMol = _computeDistanceMatrix( mobileMoleculeResiduePositions );
+		const VTX::Util::Math::Matrix<float> distanceMatrixStaticMol
+			= computeDistanceMatrix( staticMoleculeResiduePositions );
+		const VTX::Util::Math::Matrix<float> distanceMatrixMobileMol
+			= computeDistanceMatrix( mobileMoleculeResiduePositions );
 
 		// Calculate CE similarities
-		const Math::Matrix<float> scoreMatrix
-			= _computeScoreMatrix( distanceMatrixStaticMol, distanceMatrixMobileMol, correctedParameters );
+		const VTX::Util::Math::Matrix<float> scoreMatrix
+			= computeScoreMatrix( distanceMatrixStaticMol, distanceMatrixMobileMol, correctedParameters );
 
 		// Calculate Top N Paths
 		const std::vector<Path> bestPaths
@@ -115,33 +181,4 @@ namespace VTX::Tool::Structalign::backend
 		return result;
 	}
 
-	std::vector<Vec3f> CEAlign::_generateResiduePositionsVector(
-		const Model::Molecule &	 p_molecule,
-		const CustomParameters & p_parameters
-	)
-	{
-		std::vector<Vec3f> residuePositionsVector = std::vector<Vec3f>();
-		residuePositionsVector.resize( p_molecule.getResidueCount() );
-
-		size_t index = 0;
-
-		for ( uint iResidue = 0; iResidue < p_molecule.getResidueCount(); iResidue++ )
-		{
-			const Model::Residue * const residuePtr = p_molecule.getResidue( iResidue );
-
-			bool considerResidue = residuePtr != nullptr;
-			considerResidue		 = considerResidue && ( p_parameters.considerWater || !residuePtr->isWater() );
-			considerResidue		 = considerResidue && ( p_parameters.considerHiddenResidue || residuePtr->isVisible() );
-
-			if ( considerResidue )
-			{
-				residuePositionsVector[ index ] = _computeResidueCenterOfMass( *residuePtr );
-				index++;
-			}
-		}
-
-		residuePositionsVector.resize( index );
-
-		return residuePositionsVector;
-	}
 } // namespace VTX::Tool::Structalign::backend
